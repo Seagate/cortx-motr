67e182238d30b1790638cfab9bf33a24bc799088
Vikram Jadhav <vikram.jadhav@seagate.com>
2020-10-05 07:52:15 -0600

    Fixed unknown symbol warning.

--- fop/fom.c
+++ fop/fom.c
@@ -342,10 +342,12 @@ static void m0_fom_check_deadlock(const struct m0_fom *fom)
 	if (!M0_IN(fom->fo_type->ft_id, (M0_CAS_PUT_FOP_OPCODE,
 					 M0_CAS_DEL_FOP_OPCODE)))
 	    return;
+#ifndef __KERNEL__
 	if (m0_cas_fom_in_deadlock(fom)) {
 		M0_LOG(M0_ERROR, "found deadlock with fom %p", fom);
 		M0_IMPOSSIBLE("deadlock");
 	}
+#endif
 }
 
 /*
