2cb24e804d7d629929a6289d9b5031c9dfa3ef77
Bansidhar Soni <bansidhar.soni@seagate.com>
2021-01-11 01:14:59 -0700

    mini prov: lvm creation and required KV added
    - confstore call TODO added for lvm params
    
    Signed-off-by: Bansidhar Soni <bansidhar.soni@seagate.com>

--- utils/mini_provisioner/motr_mini_prov.py
+++ utils/mini_provisioner/motr_mini_prov.py
@@ -24,7 +24,7 @@ motr_url = 'json:///home/743120/mini_provisioner/motr_prov_conf.json'
 def set_repo():
     repo_arr = [
                   {
-                   "file":"/etc/yum.repos.d/cortx_commons.repo", 
+                   "file":"/etc/yum.repos.d/cortx_commons.repo",
                    "desc":"[cortx_commons]",
                    "name":"cortx_commons",
                    "gpgcheck":"0",
@@ -101,7 +101,7 @@ def configure_lnet_manual():
     fp.write("options lnet networks=tcp({})  config_on_load=1  lnet_peer_discovery_disabled=1\n".format(iface))
     fp.close()
     start_services(["lnet"])
-    
+
     #Update conf store
     load_config('motr_prov_conf', motr_url)
     lnet_conf = conf_store.get('motr_prov_conf', 'motr>lnet', default_val=None)
@@ -136,7 +136,7 @@ def configure_lnet_from_conf_store():
     ret = execute_command(cmd, 180)
     return ret
 
-def execute_command(cmd, timeout_secs):
+def execute_command(cmd, timeout_secs = 100):
     ps = subprocess.Popen(cmd, stdin=subprocess.PIPE,
                           stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                           shell=True)
@@ -157,12 +157,85 @@ def execute_command_1(cmd):
 def lnet_conf_check():
   cmd = "rpm -qa | grep lustre"
   (ret, stdout)= execute_command(cmd)
-  print("Atul ret={}\n\nstdout={}".format(ret, type(stdout)))    
+  print("Atul ret={}\n\nstdout={}".format(ret, type(stdout)))
 
 def load_config(index, backend_url):
     """Instantiate and Load Config into constore"""
     conf_store.load(index, backend_url)
     return conf_store
+
+def create_lvm(node_name, metadata_dev, is_physical):
+    cmd = f"swapoff -a"
+    execute_command(cmd)
+
+    cmd = f"parted {metadata_dev} mklabel gpt"
+    execute_command(cmd)
+
+    end = 10
+    if is_physical:
+        end = 1000
+    cmd = f"parted {metadata_dev} mkpart primary ext4 0% {end}GB"
+    execute_command(cmd)
+
+    start = 11
+    if is_physical:
+        start = 1001
+    cmd = f"parted {metadata_dev} mkpart primary ext2 {start}GB 100%"
+    execute_command(cmd)
+
+    cmd = f"parted {metadata_dev} toggle 2 lvm"
+    execute_command(cmd)
+
+    cmd = f"pvcreate {metadata_dev}2"
+    execute_command(cmd)
+
+    cmd = f"vgcreate  vg_metadata_{node_name} {metadata_dev}2"
+    execute_command(cmd)
+
+    cmd = f"vgchange --addtag {node_name} vg_metadata_{node_name}"
+    execute_command(cmd)
+
+    cmd = f"vgscan --cache"
+    execute_command(cmd)
+
+    cmd = f"lvcreate -n lv_main_swap vg_metadata_{node_name} -l 51%VG"
+    execute_command(cmd)
+
+    cmd = f"lvcreate -n lv_raw_metadata vg_metadata_{node_name} -l 100%FREE"
+    execute_command(cmd)
+
+    cmd = f"mkswap -f /dev/vg_metadata_{node_name}/lv_main_swap"
+    execute_command(cmd)
+
+    cmd = f"test -e /dev/vg_metadata_{node_name}/lv_main_swap"
+    execute_command(cmd)
+
+    cmd = f"swapon /dev/vg_metadata_{node_name}/lv_main_swap"
+    execute_command(cmd)
+
+    cmd = (
+            f"echo \"/dev/vg_metadata_{node_name}/lv_main_swap    swap    "
+            f"swap    defaults        0 0\" >> /etc/fstab"
+        )
+    execute_command(cmd)
+
+    cmd = f"mkfs.ext4 {metadata_dev}1 -L cortx_metadata"
+    execute_command(cmd)
+
+    #cmd = f"blockdev --flushbufs /dev/disk/by-id/dm-name-mpath* || true"
+    #execute_command(cmd)
+
+    time.sleep(10)
+
+    cmd = f"timeout -k 10 30 partprobe || true"
+    execute_command(cmd)
+
+def config_lvm():
+    #TODO: get below config from KV store
+    node_name = "srvnode-2"
+    metadata_device = "/dev/sde"
+    is_physical = False
+    create_lvm(node_name, metadata_device, is_physical)
 
 if __name__ == "__main__":
     #load_config('motr_prov_conf', 'json:///home/743120/mini_provisioner/motr_prov_conf.json')
