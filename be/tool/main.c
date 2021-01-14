5cfda07c8b6fe2f78f7ab3eae7a99c8f3ed0659a
Kanchan Chaudhari <kanchan.chaudhari@seagate.com>
2021-01-14 01:11:09 -0700

    EOS-16241: Increase parallelization in accessing EMAP btrees
    Changes:
    Added an array (currently 256 entries) of m0_be_emap in
    struct m0_stob_ad_domain which is used as hash table where each entry will
    point to one emap btree.
    The #define EMAP_HT_SIZE is used to define the size of hash table which is
    power of 2 and limited to 65536.
    Used m0_fid_hash() to get the hash on the ek_prefix from struct m0_be_emap_key.
    Then did mod 256 on the returned hash to use that as array index.
    
    Signed-off-by: Kanchan Chaudhari <kanchan.chaudhari@seagate.com>

--- be/tool/main.c
+++ be/tool/main.c
@@ -108,24 +108,30 @@ void track_cob_btrees(struct m0_cob_domain *cdom, bool print_btree)
 void track_ad_btrees(struct stob_ad_0type_rec *rec, bool print_btree)
 {
 	struct m0_balloc         *m0balloc;
+        int                       i;
 
 	m0balloc = container_of(rec->sa0_ad_domain->sad_ballroom,
 				struct m0_balloc, cb_ballroom);
 
 	if (print_btree) {
-		M0_LOG(M0_ALWAYS, "em_mapping");
-		btree_dbg_print(&rec->sa0_ad_domain->sad_adata.em_mapping);
-		M0_LOG(M0_ALWAYS, "grp_exts");
-		btree_dbg_print(&m0balloc->cb_db_group_extents);
-		M0_LOG(M0_ALWAYS, "grp_dsc");
-		btree_dbg_print(&m0balloc->cb_db_group_desc);
-	} else
+                for (i = 0; i < EMAP_HT_SIZE; i++) {
+		        M0_LOG(M0_ALWAYS, "em_mapping %d",i);
+		        btree_dbg_print(&rec->sa0_ad_domain->sad_adata_ht[i].sad_adata.em_mapping);
+                }
+		        M0_LOG(M0_ALWAYS, "grp_exts");
+		        btree_dbg_print(&m0balloc->cb_db_group_extents);
+                        M0_LOG(M0_ALWAYS, "grp_dsc");
+                        btree_dbg_print(&m0balloc->cb_db_group_desc);
+	} else {
+                for (i = 0; i < EMAP_HT_SIZE; i++) {
 		M0_LOG(M0_ALWAYS,"M0_BE:AD em_mapping = %p"
 				 "cb_db_group_extents btree= %p "
 				 "cb_db_group_desc btree= %p",
-				 &rec->sa0_ad_domain->sad_adata.em_mapping,
+				 &rec->sa0_ad_domain->sad_adata_ht[i].sad_adata.em_mapping,
 				 &m0balloc->cb_db_group_extents,
 				 &m0balloc->cb_db_group_desc);
+                }
+        }
 
 }
 static void scan_btree(struct m0_be_domain *dom, bool print_btree)
@@ -248,6 +254,7 @@ int main(int argc, char *argv[])
 	char                    *path;
 	int                      rc;
 
+	// m0_node_uuid_string_set(NULL);
 	if (argc > 1 && m0_streq(argv[1], "be_recovery_run")) {
 		path = argc > 2 ? argv[2] : NULL;
 		be_recovery_run(path);
