85579069f4d83ebb798fdc81f2da02edd1d4335f
Atul Deshmukh <atul.deshmukh@seagate.com>
2021-01-15 00:54:12 -0700

    Motr mini prov new format

--- scripts/install/opt/seagate/cortx/motr/bin/motr_mini_prov.py
+++ scripts/install/opt/seagate/cortx/motr/bin/motr_mini_prov.py
@@ -1,190 +1,230 @@
 #!/usr/bin/env python3
-
-import subprocess
-import os
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
 import sys
-import getopt
-import json
-import time
-from socket import gethostname
-from typing import Dict, List, NamedTuple, Set
+import errno
+import argparse
+import inspect
+import traceback
+import os
 import re
-from datetime import datetime
-from collections import OrderedDict
-lnet_info = {}
+import subprocess
+import time
+from cortx.utils.process import SimpleProcess
+from cortx.utils.conf_store import Conf
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
-from cortx.utils.schema.payload import Json
-from cortx.utils.conf_store import ConfStore
-
+MOTR_KERNEL_FILE = "/lib/modules/{kernel_ver}/kernel/fs/motr/m0tr.ko"
+MOTR_SYS_FILE = "/etc/sysconfig/motr"
+MOTR_CONFIG_SCRIPT = "/opt/seagate/cortx/motr/libexec/motr_cfg.sh"
 LNET_CONF_FILE = "/etc/modprobe.d/lnet.conf"
-TIMEOUT = 180
+SYS_CLASS_NET_DIR = "/sys/class/net/"
+SLEEP_SECS = 2
+TIMEOUT_SECS = 120
 
-class motr_prov:
-    def __init__(self, index, url):
-        self.conf_store = ConfStore()
-        self.url = url
-        self.index = index
-        self.load_config(self.index, self.url)
-        self.server_id = int(self.conf_store.get(self.index, 'cluster>current>server_id', default_val=None))
+class MotrError(Exception):
+    """ Generic Exception with error code and output """
 
-    def configure_lnet_from_conf_store(self):
-        '''
-           Get iface and /etc/modprobe.d/lnet.conf params from
-           conf store. Configure lnet. Start lnet service
-        '''
-        lnet_conf = self.conf_store.get(self.index, f'cluster>server[{self.server_id}]>network>motr_net', default_val=None)
-        with open(LNET_CONF_FILE, "w") as fp:
-            fp.write(f"options lnet networks={lnet_conf['interface_type']}({lnet_conf['interface']})  config_on_load=1  lnet_peer_discovery_disabled=1\n")
-        time.sleep(10)
-        self.start_services(["lnet"])
+    def __init__(self, rc, message, *args):
+        self._rc = rc
+        self._desc = message % (args)
+        sys.stderr.write("error(%d): %s\n" %(self._rc, self._desc))
 
-    def execute_command(self, cmd, timeout_secs):
+    def __str__(self):
+        if self._rc == 0: return self._desc
+        return "error(%d): %s" %(self._rc, self._desc)
+
+
+def execute_command(cmd, timeout_secs):
         
-        ps = subprocess.Popen(cmd, stdin=subprocess.PIPE,
-                              stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                              shell=True)
-        stdout, stderr = ps.communicate(timeout=timeout_secs);
-        stdout = str(stdout, 'utf-8')
-        sys.stdout.write(f"[CMD] {cmd}\n")
-        sys.stdout.write(f"[OUT]\n{stdout}\n")
-        sys.stdout.write(f"[RET] {ps.returncode}\n")
-        if ps.returncode != 0:
-            sys.exit(ps.returncode)
-        else:
-            return stdout
-                 
-    def start_services(self, services):
-        for service in services:
-            cmd = "service {} start".format(service)
-            sys.stdout.write("Executing cmd = {}\n".format(cmd))
-            self.execute_command(cmd, TIMEOUT)
-            time.sleep(10)
-            cmd = "service {} status".format(service)
-            sys.stdout.write("Executing cmd = {}\n".format(cmd))
-            self.execute_command(cmd, TIMEOUT)
-            time.sleep(10)
+    ps = subprocess.Popen(cmd, stdin=subprocess.PIPE,
+                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                          shell=True)
+    stdout, stderr = ps.communicate(timeout=timeout_secs);
+    stdout = str(stdout, 'utf-8')
+    sys.stdout.write(f"[CMD] {cmd}\n")
+    sys.stdout.write(f"[OUT]\n{stdout}\n")
+    sys.stdout.write(f"[RET] {ps.returncode}\n")
+    return stdout, ps.returncode
 
-    def load_config(self, index, backend_url):
-        """Instantiate and Load Config into constore"""
-        self.conf_store.load(index, backend_url)
-        return self.conf_store
+def start_services(services):
+    for service in services:
+        cmd = "service {} start".format(service)
+        execute_command(cmd, TIMEOUT_SECS)
+        cmd = "service {} status".format(service)
+        execute_command(cmd, TIMEOUT_SECS)
 
-    def create_lvm(self, node_name, metadata_dev, is_physical):
+def validate_file(file):
+    if not os.path.exists(file):
+        raise MotrError(errno.ENOENT, "{} not exist".format(file))
 
-        self.validate_file(metadata_dev)
+def validate_motr_rpm(self):
+    try:
+        cmd = f"uname -r"
+        cmd_res = execute_command(cmd, TIMEOUT_SECS)
+        op = cmd_res[0]
+        kernel_ver = op.replace('\n', '')
+        kernel_module = f"/lib/modules/{kernel_ver}/kernel/fs/motr/m0tr.ko"
+        sys.stdout.write(f"[INFO] Checking for {kernel_module}\n")
+        validate_file(kernel_module)
+        sys.stdout.write(f"[INFO] Checking for {MOTR_SYS_FILE}\n")
+        validate_file(MOTR_SYS_FILE)
+    except MotrError as e:
+        pass
+
+def motr_config(self):
+    server_id = int(Conf.get(self._index, 'cluster>current>server_id'))
+    node_type = Conf.get(self._index, f'cluster>server')[server_id]['node_type']
+    is_physical = True if node_type == "HW" else False
+    sys.stdout.write(f"[INFO] server_id={server_id} node_type={node_type} is_physical={is_physical}\n")
+    if is_physical:
+        execute_command(MOTR_CONFIG_SCRIPT, TIMEOUT_SECS)
+
+def configure_net(self):
+     server_id = int(Conf.get(self._index, 'cluster>current>server_id'))
+     xport = Conf.get(self._index,
+                          f'cluster>server')[server_id]['network']['motr_net']['transport']
+     sys.stdout.write(f"[INFO] transport type = {xport}\n")
+     if (xport == "lnet"):
+        configure_lnet_from_conf_store(self)
+
+def configure_lnet_from_conf_store(self):
+    '''
+       Get iface and /etc/modprobe.d/lnet.conf params from
+       conf store. Configure lnet. Start lnet service
+    '''
+    server_id = int(Conf.get(self._index, 'cluster>current>server_id'))
+    lnet_conf = Conf.get(self._index,
+                         f'cluster>server')[server_id]['network']['motr_net']
+    iface_type = lnet_conf['interface_type']
+    iface = lnet_conf['interface']
+    sys.stdout.write(f"[INFO] {iface_type}=({iface})") 
+    sys.stdout.write(f"[INFO] Updating {LNET_CONF_FILE}")
+    with open(LNET_CONF_FILE, "w") as fp:
+        fp.write(f"options lnet networks={iface_type}({iface}) config_on_load=1  lnet_peer_discovery_disabled=1\n")
+        time.sleep(SLEEP_SECS)
+        start_services(["lnet"])
+
+
+def create_lvm(node_name, metadata_dev, is_physical):
+    try:
+        validate_file(metadata_dev)
 
         cmd = f"fdisk -l {metadata_dev}"
-        op = self.execute_command(cmd, TIMEOUT)
+        execute_command(cmd, TIMEOUT_SECS)
 
         cmd = f"swapoff -a"
-        self.execute_command(cmd, TIMEOUT)
+        execute_command(cmd, TIMEOUT_SECS)
 
         cmd = f"pvcreate {metadata_dev}"
-        self.execute_command(cmd, TIMEOUT)
+        execute_command(cmd, TIMEOUT_SECS)
 
         cmd = f"vgcreate  vg_metadata_{node_name} {metadata_dev}"
-        self.execute_command(cmd, TIMEOUT)
+        execute_command(cmd, TIMEOUT_SECS)
 
         cmd = f"vgchange --addtag {node_name} vg_metadata_{node_name}"
-        self.execute_command(cmd, TIMEOUT)
+        execute_command(cmd, TIMEOUT_SECS)
 
         cmd = f"vgscan --cache"
-        self.execute_command(cmd, TIMEOUT)
+        execute_command(cmd, TIMEOUT_SECS)
 
         cmd = f"lvcreate -n lv_main_swap vg_metadata_{node_name} -l 51%VG"
-        self.execute_command(cmd, TIMEOUT)
+        execute_command(cmd, TIMEOUT_SECS)
 
         cmd = f"lvcreate -n lv_raw_metadata vg_metadata_{node_name} -l 100%FREE"
-        self.execute_command(cmd, TIMEOUT)
+        execute_command(cmd, TIMEOUT_SECS)
 
         cmd = f"mkswap -f /dev/vg_metadata_{node_name}/lv_main_swap"
-        self.execute_command(cmd, TIMEOUT)
+        execute_command(cmd, TIMEOUT_SECS)
 
         cmd = f"test -e /dev/vg_metadata_{node_name}/lv_main_swap"
-        self.execute_command(cmd, TIMEOUT)
+        execute_command(cmd, TIMEOUT_SECS)
 
         cmd = f"swapon /dev/vg_metadata_{node_name}/lv_main_swap"
-        self.execute_command(cmd, TIMEOUT)
+        execute_command(cmd, TIMEOUT_SECS)
 
         cmd = (
-            f"echo \"/dev/vg_metadata_{node_name}/lv_main_swap    swap    "
-            f"swap    defaults        0 0\" >> /etc/fstab"
+           f"echo \"/dev/vg_metadata_{node_name}/lv_main_swap    swap    "
+           f"swap    defaults        0 0\" >> /etc/fstab"
         )
-        self.execute_command(cmd, TIMEOUT)
+        execute_command(cmd, TIMEOUT_SECS)
+    except:
+        pass
 
+def config_lvm(self):
+    server_id = int(Conf.get(self._index, 'cluster>current>server_id'))
+    node_name = Conf.get(self._index, f'cluster>server')[server_id]['hostname']
+    node_name = node_name.split('.')[0]
+    metadata_device = Conf.get(self._index, f'cluster>server')[server_id]['storage']['metadata_devices']
+    node_type = Conf.get(self._index, f'cluster>server')[server_id]['node_type']
+    sys.stdout.write(f"[INFO] server_id={server_id} node_name={node_name} node_type={node_type} metadata_device={metadata_device}\n")
+    is_physical = True if node_type == "HW" else False
+    create_lvm(node_name, metadata_device[0], is_physical)
 
-    def config_lvm(self):
-        node_name = self.conf_store.get(self.index, f'cluster>server[{self.server_id}]>hostname', default_val=None)
-        node_name = node_name.split('.')[0]
-        metadata_device = self.conf_store.get(self.index,
-        f'cluster>server[{self.server_id}]>storage>metadata_devices', default_val=None)
-        is_physical = True if self.conf_store.get(self.index, f'cluster>server[{self.server_id}]>node_type', default_val=None) == "HW" else False
+def get_lnet_xface() -> str:
+    lnet_xface = None
+    try:
+        with open(LNET_CONF_FILE, 'r') as f:
+            # Obtain interface name
+            for line in f.readlines():
+                if len(line.strip()) <= 0: continue
+                tokens = re.split(r'\W+', line)
+                if len(tokens) > 4:
+                    lnet_xface = tokens[4]
+                    break
+    except:
+        pass
 
-        self.create_lvm(node_name, metadata_device[0], is_physical)
+    if lnet_xface == None:
+        raise MotrError(errno.EINVAL, "Cant obtain iface details from %s", LNET_CONF_FILE)
+    if lnet_xface not in os.listdir(SYS_CLASS_NET_DIR):
+        raise MotrError(errno.EINVAL, "Invalid iface %s in lnet.conf", lnet_xface)
 
-    def config_motr(self):
-        is_physical = True if self.conf_store.get(self.index, f'cluster>server[{self.server_id}]>node_type', default_val=None) == "HW" else False
-        if is_physical:
-            cmd = f"/opt/seagate/cortx/motr/libexec/motr_cfg.sh"
-            self.execute_command(cmd, TIMEOUT)
+    return lnet_xface
 
-    def validate_motr_rpm(self):
-        cmd = f"uname -r"
-        op = self.execute_command(cmd, TIMEOUT)
-        kernel_ver = op.replace('\n', '')
+def check_pkgs(src_pkgs, dest_pkgs):
+    missing_pkgs = []
+    for src_pkg in src_pkgs:
+        found = False
+        for dest_pkg in dest_pkgs:
+            if src_pkg in dest_pkg:
+                found = True
+                break
+        if found == False:
+            missing_pkgs.append(src_pkg)
+    if missing_pkgs:
+        raise MotrError(errno.ENOENT, f'Missing pkgs: {missing_pkgs}')
 
-        self.validate_file(f"/lib/modules/{kernel_ver}/kernel/fs/motr/m0tr.ko")
+def test_lnet(self):
+    search_lnet_pkgs = ["kmod-lustre-client", "lustre-client"]
 
-        self.validate_file("/etc/sysconfig/motr")
-
-    def validate_file(self, file):
-        if not os.path.exists(file):
-            sys.stderr.write(f"[ERR] {file} does not exist.\n")
-            sys.exit(1)
-        else:
-            sys.stdout.write(f"[MSG] {file} exists.\n")
-
-    def test_lnet(self):
-        missing_pkgs = []
-        LNET_CONF_FILE="/etc/modprobe.d/lnet.conf"
-        search_lnet_pkgs = ["kmod-lustre-client", "lustre-client"]
-
+    try:
         # Check missing luster packages
-        cmd = "rpm -qa | grep lustre"
-        temp =  self.execute_command(cmd, TIMEOUT)
+        cmd = f'rpm -qa | grep lustre'
+        cmd_res = execute_command(cmd, TIMEOUT_SECS)
+        temp = cmd_res[0]
         lustre_pkgs = list(filter(None, temp.split("\n")))
-        for pkg in search_lnet_pkgs:
-            found = False
-            for lustre_pkg in lustre_pkgs:
-                if pkg in lustre_pkg:
-                    found = True
-                    break;
-            if found == False:
-                missing_pkgs.append(pkg)
-        if missing_pkgs:
-            sys.stderr.write("Missing pkgs ={}\n".format(missing_pkgs))
+        check_pkgs(search_lnet_pkgs, lustre_pkgs)
 
-        # Check for lnet config file
-        if os.path.exists(LNET_CONF_FILE):
-            with open(LNET_CONF_FILE) as fp:
-                line = fp.readline()
-                while line:
-                    tokens = line.split(' ')
-                    # Get lnet iface
-                    cmd = 'echo \'{}\' | cut -d "(" -f2 | cut -d ")" -f1'.format(tokens[2])
-                    device = self.execute_command(cmd, TIMEOUT)
-                    device = device.strip('\n')
-                    sys.stdout.write("iface:{}\n".format(device))
-
-                    # Get ip of iface
-                    cmd = "ifconfig {} | awk \'/inet /\'".format(device)
-                    ipconfig_op = self.execute_command(cmd, TIMEOUT)
-                    ip = list(ipconfig_op.split())[1]
-                    sys.stdout.write("ip = {}\n".format(ip))
-
-                    # Ping ip
-                    cmd = "ping -c 3 {}".format(ip)
-                    op = self.execute_command(cmd, TIMEOUT)
-                    sys.stdout.write("{}\n".format(op))
-                    line = fp.readline()
+        lnet_xface = get_lnet_xface()
+        ip_addr = os.popen(f'ip addr show {lnet_xface}').read().split("inet ")[1].split("/")[0]
+        cmd = "ping -c 3 {}".format(ip_addr)
+        cmd_res = execute_command(cmd, TIMEOUT_SECS)
+        sys.stdout.write("{}\n".format(cmd_res[0]))
+    except MotrError as e:
+        pass
