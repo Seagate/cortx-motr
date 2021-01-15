d162a788fdfa84297de687ddecb23277849cb0ac
Upendra Patwardhan <upendra.patwardhan@seagate.com>
2021-01-15 16:10:01 +0530

    EOS 15847, EOS 14947 Libfabric : Rewrite domain init. Implement Transfer machine init and fini using libfabric API's. (#307)
    * EOS-14890:Implement domain_init using libfabric APIs
    
    Implementation of domain_init
    
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>
    
    * EOS-14941 Libfabric:Added changes for domain_fini (#279)
    
    Signed-off-by: Jaydeep Mohite <jaydeep.mohite@seagate.com>
    
    Co-authored-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>
    
    * Add waitset and pollset to transfer machine for event queue and completion queue monitoring
    
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>
    
    * Add event and completion monitoring in poller thread
    
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>
    
    * Correct the array size in fi_poll call
    
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>
    
    * Fix bug in libfab_ep_addr_decode
    
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>
    
    * EOS-15860, EOS-15861 Libfabric: Added buffer event notify and buffer, segment size related API's (#313)
    
    * Updated buffer event notify and buffer, segment size related API's
    
    Signed-off-by: Jaydeep Mohite <jaydeep.mohite@seagate.com>
    
    * EOS-14948 Libfabric : Implement transfer machine start and stop using libfabric APIs (#318)
    
    * Implement transfer machine start and stop.
    
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>
    
    * Add tm lock and unlock calls to libfab_ma_fini.
    
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>
    
    * EOS-14949 Libfabric: Added buf_del() API and poller thread (#314)
    
    * Added buffer delete operation and event, completion queue handling
    * Added connection event monitoring mechanism
    * Event count validation added
    Signed-off-by: Jaydeep Mohite <jaydeep.mohite@seagate.com>
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>
    Co-authored-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>
    
    * Addedin locking mechanism in poller thread
    
    Signed-off-by: Jaydeep Mohite <jaydeep.mohite@seagate.com>
    
    * Add tm lock and unlock in tm_start and remove FI_SOURCE flag from fi_getinfo call in libfab_passive_ep_create.
    
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>
    
    * Fixed merge conflict in init.c
    
    Signed-off-by: Jaydeep Mohite <jaydeep.mohite@seagate.com>
    
    * Fixed merge conflict in configure files
    
    Signed-off-by: Jaydeep Mohite <jaydeep.mohite@seagate.com>
    
    Co-authored-by: Jaydeep Mohite <jaydeep.mohite@seagate.com>
    Co-authored-by: VinothVijayakumar-dev <vinoth.v@seagate.com>

--- net/libfab/libfab_internal.h
+++ net/libfab/libfab_internal.h
@@ -24,11 +24,13 @@
 
 #ifndef __MOTR_NET_LIBFAB_LIBFAB_INTERNAL_H__
 #define __MOTR_NET_LIBFAB_LIBFAB_INTERNAL_H__
+#include <netinet/in.h>                    /* INET_ADDRSTRLEN */
 
-#include "rdma/fi_eq.h"
-#include "rdma/fi_domain.h"
-#include "rdma/fi_endpoint.h"
 #include "rdma/fabric.h"
+#include "rdma/fi_cm.h"
+#include "rdma/fi_domain.h"
+#include "rdma/fi_eq.h"
+#include "rdma/fi_endpoint.h"
 
 extern struct m0_net_xprt m0_net_libfab_xprt;
 
@@ -39,23 +41,51 @@ extern struct m0_net_xprt m0_net_libfab_xprt;
  */
 
 /**
- *    Libfabric parameters.
- * 
- */
-struct m0_fab__dom_param {
-	struct fi_info       *fdp_fi;      /* Fabric info */
-	struct fid_fabric    *fdp_fabric;  /* Fabric fid */
-	struct fid_domain    *fdp_domain;  /* Domain fid */
-};
-
-
-/**
  *    Private data pointed to by m0_net_buffer::nb_xprt_private.
  * 
  */
-struct m0_fab__buf_params {
-        struct m0_net_buffer   *fbp_nb; /* Pointer back to the network buffer */
-        struct fid_mr          *fbp_mr; /* Libfab memory region */
+struct m0_fab__buf {
+	uint64_t               fb_magic;  /* Magic number */
+	struct m0_net_buffer  *fb_nb;     /* Pointer back to network buffer*/
+	struct fid_mr         *fb_mr;     /* Libfab memory region */
+	struct m0_tlink        fb_linkage;/* Linkage in list of completed bufs*/
+	m0_bindex_t            fb_length; /* Total size of data to be received*/
+};
+
+struct m0_fab__ep_name {
+	char fen_addr[INET6_ADDRSTRLEN];       /*  */
+	char fen_port[6];                      /* Port range 0-65535 */
+};
+
+struct m0_fab__ep_res {
+	struct fid_av           *fer_av;       /* Address vector */
+	struct fid_eq           *fer_eq;       /* Event queue */
+	struct fid_cq           *fer_tx_cq;    /* Transmit Completion Queue */
+	struct fid_cq           *fer_rx_cq;    /* Recv Completion Queue */
+	struct fid_cntr         *fer_tx_cntr;  /* Transmit Counter */
+	struct fid_cntr         *fer_rx_cntr;  /* Recv Counter */
+};
+
+struct m0_fab__ep {
+	struct m0_net_end_point  fep_nep;      /* linked into a per-tm list */
+	struct m0_fab__ep_name   fep_name;     /* "addr:port" in str format */
+	struct fi_info          *fep_fi;       /* Fabric interface info */
+	struct fid_fabric       *fep_fabric;   /* Fabric fid */
+	struct fid_domain       *fep_domain;   /* Domain fid */
+	struct fid_ep           *fep_ep;       /* Active Endpoint */
+	struct fid_pep          *fep_pep;      /* Passive endpoint */
+	struct m0_fab__ep_res    fep_ep_res;   /* Endpoint resources */
+};
+
+struct m0_fab__tm {
+	struct m0_net_transfer_mc *ftm_net_ma;  /* Generic transfer machine */
+	struct m0_thread           ftm_poller;  /* Poller thread */
+	int                        ftm_epollfd; /* epoll(2) file descriptor. */
+	struct fid_poll           *ftm_pollset;
+	struct fid_wait           *ftm_waitset;
+	struct m0_fab__ep         *ftm_pep;     /* Passive ep(listening mode) */
+	bool                       ftm_shutdown;/* tm Shutdown flag */
+	struct m0_tl               ftm_done;    /* List of completed buffers */
 };
 
 /** @} end of netlibfab group */
