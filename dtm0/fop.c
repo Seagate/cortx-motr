9a25316439f40b02463a8ca48765c3b548f1d1e9
Anatoliy Bilenko <anatoliy.bilenko@seagate.com>
2021-01-17 11:53:53 +0000

    Initial service commit

---
+++ dtm0/fop.c
@@ -0,0 +1,192 @@
+/* -*- C -*- */
+/*
+ * Copyright (c) 2012-2020 Seagate Technology LLC and/or its Affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For any questions about this software or licensing,
+ * please email opensource@seagate.com or cortx-questions@seagate.com.
+ *
+ */
+
+
+#include "lib/errno.h"
+#include "lib/assert.h"
+#include "lib/memory.h"
+#include "lib/chan.h"
+#include "lib/finject.h"
+#include "lib/time.h"
+#include "lib/misc.h"           /* M0_IN() */
+#include "fop/fop.h"
+#include "fop/fom.h"
+#include "fop/fom_generic.h"
+
+#include "rpc/rpc.h"
+#include "rpc/rpclib.h"
+#include "fop/fop_item_type.h"
+
+#include "dtm0/fop.h"
+#include "dtm0/fop_xc.h"
+#include "rpc/rpc_opcodes.h"
+
+static void dtm0_rpc_item_reply_cb(struct m0_rpc_item *item);
+
+/*
+  RPC item operations structures.
+ */
+const struct m0_rpc_item_ops dtm0_req_fop_rpc_item_ops = {
+        .rio_replied = dtm0_rpc_item_reply_cb,
+};
+
+struct m0_fop_type dtm0_req_fop_fopt;
+struct m0_fop_type dtm0_rep_fop_fopt;
+
+/*
+  Fom specific routines for corresponding fops.
+ */
+static int dtm0_fom_tick(struct m0_fom *fom);
+static int dtm0_req_fop_fom_create(struct m0_fop *fop, struct m0_fom **out,
+				   struct m0_reqh *reqh);
+static void dtm0_fom_fini(struct m0_fom *fom);
+static size_t dtm0_fom_locality(const struct m0_fom *fom);
+
+static const struct m0_fom_ops dtm0_req_fop_fom_ops = {
+	.fo_fini = dtm0_fom_fini,
+	.fo_tick = dtm0_fom_tick,
+	.fo_home_locality = dtm0_fom_locality
+};
+
+extern struct m0_reqh_service_type dtm0_service_type;
+
+enum dtm0_phases {
+	DTM0_REQ_WHATEVER = M0_FOPH_NR + 1,
+};
+
+static const struct m0_fom_type_ops dtm0_req_fop_fom_type_ops = {
+        .fto_create = dtm0_req_fop_fom_create,
+};
+
+static void dtm0_rpc_item_reply_cb(struct m0_rpc_item *item)
+{
+	struct m0_fop *reply;
+
+        M0_PRE(item != NULL);
+	M0_PRE(M0_IN(m0_fop_opcode(m0_rpc_item_to_fop(item)),
+		     (M0_DTM0_REQ_OPCODE)));
+
+	if (m0_rpc_item_error(item) == 0) {
+		reply = m0_rpc_item_to_fop(item->ri_reply);
+		M0_ASSERT(M0_IN(m0_fop_opcode(reply), (M0_DTM0_REP_OPCODE)));
+	}
+}
+
+void m0_dtm0_fop_fini(void)
+{
+	m0_fop_type_fini(&dtm0_req_fop_fopt);
+	m0_fop_type_fini(&dtm0_rep_fop_fopt);
+	m0_xc_dtm0_fop_fini();
+}
+
+int m0_dtm0_fop_init(void)
+{
+	m0_xc_dtm0_fop_init();
+	M0_FOP_TYPE_INIT(&dtm0_req_fop_fopt,
+			 .name      = "DTM0 request",
+			 .opcode    = M0_DTM0_REQ_OPCODE,
+			 .xt        = dtm0_req_fop_xc,
+			 .rpc_flags = M0_RPC_MUTABO_REQ,
+			 .fom_ops   = &dtm0_req_fop_fom_type_ops,
+			 .sm        = &m0_generic_conf,
+			 .svc_type  = &dtm0_service_type);
+	M0_FOP_TYPE_INIT(&dtm0_rep_fop_fopt,
+			 .name      = "DTM0 reply",
+			 .opcode    = M0_DTM0_REP_OPCODE,
+			 .xt        = dtm0_rep_fop_xc,
+			 .rpc_flags = M0_RPC_ITEM_TYPE_REPLY,
+			 .fom_ops   = &dtm0_req_fop_fom_type_ops);
+	return 0;
+}
+
+/*
+  Allocates and initialises a fom.
+ */
+static int dtm0_req_fop_fom_create(struct m0_fop *fop,
+				   struct m0_fom **out, struct m0_reqh *reqh)
+{
+        struct m0_fom           *fom;
+	struct m0_fop           *rfop;
+
+	M0_PRE(fop != NULL);
+        M0_PRE(out != NULL);
+	M0_PRE(M0_IN(m0_fop_opcode(fop), (M0_DTM0_REQ_OPCODE)));
+
+        M0_ALLOC_PTR(fom);
+        if (fom == NULL)
+                return -ENOMEM;
+
+	rfop = m0_fop_reply_alloc(fop, &dtm0_rep_fop_fopt);
+
+	if (rfop == NULL) {
+		m0_free(fom);
+		return -ENOMEM;
+	}
+	m0_fom_init(fom, &fop->f_type->ft_fom_type,
+		    &dtm0_req_fop_fom_ops, fop, rfop, reqh);
+
+        *out = fom;
+        return 0;
+}
+
+static void dtm0_fom_fini(struct m0_fom *fom)
+{
+	M0_PRE(fom != NULL);
+
+        m0_fom_fini(fom);
+        m0_free(fom);
+}
+
+static size_t dtm0_fom_locality(const struct m0_fom *fom)
+{
+	static int locality = 0;
+	M0_PRE(fom != NULL);
+	return locality++;
+}
+
+static int dtm0_fom_tick(struct m0_fom *fom)
+{
+	int                  rc;
+	struct dtm0_req_fop *req;
+	struct dtm0_rep_fop *rep;
+
+	if (m0_fom_phase(fom) < M0_FOPH_NR) {
+		rc = m0_fom_tick_generic(fom);
+	} else {
+		req = m0_fop_data(fom->fo_fop);
+		rep = m0_fop_data(fom->fo_rep_fop);
+		rep->csr_rc = req->csr_value;
+		m0_fom_phase_set(fom, M0_FOPH_SUCCESS);
+		rc = M0_FSO_AGAIN;
+	}
+
+	return rc;
+}
+
+/*
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
---
+++ dtm0/service.c
@@ -0,0 +1,114 @@
+/* -*- C -*- */
+/*
+ * Copyright (c) 2011-2020 Seagate Technology LLC and/or its Affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For any questions about this software or licensing,
+ * please email opensource@seagate.com or cortx-questions@seagate.com.
+ *
+ */
+
+
+#include "lib/errno.h"
+#include "lib/memory.h"
+
+#include "reqh/reqh_service.h"
+#include "dtm0/fop.h"
+
+static int dtm0_service_start(struct m0_reqh_service *service);
+static void dtm0_service_stop(struct m0_reqh_service *service);
+static int dtm0_service_allocate(struct m0_reqh_service **service,
+				 const struct m0_reqh_service_type *stype);
+static void dtm0_service_fini(struct m0_reqh_service *service);
+
+
+static const struct m0_reqh_service_type_ops dtm0_service_type_ops = {
+	.rsto_service_allocate = dtm0_service_allocate
+};
+
+static const struct m0_reqh_service_ops dtm0_service_ops = {
+	.rso_start = dtm0_service_start,
+	.rso_stop  = dtm0_service_stop,
+	.rso_fini  = dtm0_service_fini
+};
+
+struct m0_reqh_service_type dtm0_service_type = {
+	.rst_name  = "M0_CST_DTM0",
+	.rst_ops   = &dtm0_service_type_ops,
+	.rst_level = M0_RS_LEVEL_LATE,
+};
+
+static int _dtm0_alloc(struct m0_reqh_service **service,
+		     const struct m0_reqh_service_type *stype,
+		     const struct m0_reqh_service_ops *ops)
+{
+	struct m0_reqh_service *s;
+
+	M0_PRE(stype != NULL && service != NULL && ops != NULL);
+
+	M0_ALLOC_PTR(s);
+	if (s == NULL)
+		return -ENOMEM;
+
+	s->rs_type = stype;
+	s->rs_ops = ops;
+	*service = s;
+
+	return 0;
+}
+
+static int dtm0_service_allocate(struct m0_reqh_service **service,
+				const struct m0_reqh_service_type *stype)
+{
+	return _dtm0_alloc(service, stype, &dtm0_service_ops);
+}
+
+static int dtm0_service_start(struct m0_reqh_service *service)
+{
+        M0_PRE(service != NULL);
+        return m0_dtm0_fop_init();
+}
+
+static void dtm0_service_stop(struct m0_reqh_service *service)
+{
+
+        M0_PRE(service != NULL);
+	m0_dtm0_fop_fini();
+}
+
+static void dtm0_service_fini(struct m0_reqh_service *service)
+{
+	M0_PRE(service != NULL);
+        m0_free(service);
+}
+
+int m0_dtm0_stype_init(void)
+{
+	return m0_reqh_service_type_register(&dtm0_service_type);
+}
+
+void m0_dtm0_stype_fini(void)
+{
+	m0_reqh_service_type_unregister(&dtm0_service_type);
+}
+
+/*
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
