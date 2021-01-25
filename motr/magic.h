1a7c906dc8c4c4b516d73c00772830454840e754
Upendra Patwardhan <upendra.patwardhan@seagate.com>
2021-01-25 10:12:50 +0530

    EOS-16638 Libfabric : Fix network-bulk ut and code cleanup. (#355)
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>

--- motr/magic.h
+++ motr/magic.h
@@ -692,6 +692,12 @@ enum m0_magic_satchel {
 	/* net/libfab.c: buf list head (bad dada dadaf) */
 	M0_NET_LIBFAB_BUF_HEAD_MAGIC = 0x33baddadadadaf77,
 
+	/* net/libfab.c: rem_comp list, rem_comp::frc_magic (fed ace dedade) */
+	M0_NET_LIBFAB_RC_MAGIC = 0x33fedacededade77,
+
+	/* net/libfab.c: buf list head (bad dada dadef) */
+	M0_NET_LIBFAB_RC_HEAD_MAGIC = 0x33baddadadadef77,
+
 	/* net/net.h: m0_nep list element, endpoint (obsessed loll) */
 	M0_NET_NEP_MAGIC = 0x330b5e55ed101177,
 
