fe7d14c9a2dd9f74de202bb79ce3846b7653d2bd
Kanchan Chaudhari <kanchan.chaudhari@seagate.com>
2021-02-11 03:12:48 -0700

    EOS-17153: data recovery orchastration failed with motr-kernel service is not starting on remote node
    Issue:
    While checking status of motr-kernel service on remote node,
    the command for checking status were executing on local node shell.
    
    Fix:
    Used escape "\" char while passing command to remote node to ignore the
    special meaning for symbol "`".
    
    Signed-off-by: Kanchan Chaudhari <kanchan.chaudhari@seagate.com>

--- scripts/install/opt/seagate/cortx/motr/libexec/motr_data_recovery.sh
+++ scripts/install/opt/seagate/cortx/motr/libexec/motr_data_recovery.sh
@@ -433,7 +433,7 @@ replay_logs_and_get_gen_id_of_seg0() {
             run_cmd_on_remote_node "systemctl start motr-kernel.service"; exit_code=$?;
             [[ $exit_code == 0 ]] && break; (( cnt+=1 ));
         done
-        if ! run_cmd_on_remote_node "[[ `systemctl is-active motr-kernel.service` == 'active' ]]";then
+        if ! run_cmd_on_remote_node "[[ \`systemctl is-active motr-kernel.service\` == 'active' ]]";then
             die "motr-kernel service is not starting on remote node, cannot proceed recovery further."
         fi
     fi
@@ -804,7 +804,7 @@ run_becktool() {
             run_cmd_on_remote_node "systemctl start motr-kernel.service"; exit_code=$?;
             [[ $exit_code == 0 ]] && break; (( cnt+=1 ));
         done
-        if ! run_cmd_on_remote_node "[[ `systemctl is-active motr-kernel.service` == 'active' ]]";then
+        if ! run_cmd_on_remote_node "[[ \`systemctl is-active motr-kernel.service\` == 'active' ]]";then
         die "motr-kernel service is not starting on remote node, cannot proceed recovery further."
         fi
     fi
