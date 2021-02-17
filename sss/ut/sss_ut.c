ebeb795c007f6264cbd8947329c2149e9bd55013
Papan Kumar Singh <papan.kumarsingh@seagate.com>
2021-02-16 21:46:15 -0700

    Revert back unwanted changes.
    Signed-off-by: Papan Kumar Singh <papan.kumarsingh@seagate.com>

--- sss/ut/sss_ut.c
+++ sss/ut/sss_ut.c
@@ -85,6 +85,7 @@ static void rpc_client_and_server_start(void)
 	int rc;
 
 	M0_SET0(&sctx.rsx_motr_ctx);
+
 	sctx.rsx_xprts = m0_net_all_xprt_get();
 	sctx.rsx_xprts_nr = m0_net_xprt_nr();
 	rc = m0_net_domain_init(&client_net_dom, m0_net_xprt_default_get());
