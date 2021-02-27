0173bf26206e1d720005101d792b309307375122
Nikita Danilov <nikita.danilov@seagate.com>
2021-02-27 12:06:57 +0300

    btree: add initial pseudo-code.

---
+++ btree/internal.h
@@ -0,0 +1,64 @@
+/* -*- C -*- */
+/*
+ * Copyright (c) 2013-2020 Seagate Technology LLC and/or its Affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For any questions about this software or licensing,
+ * please email opensource@seagate.com or cortx-questions@seagate.com.
+ *
+ */
+
+#pragma once
+
+#ifndef __MOTR_BTREE_INTERNAL_H__
+#define __MOTR_BTREE_INTERNAL_H__
+
+#include "sm/op.h"
+
+/**
+ * @defgroup btree
+ *
+ * @{
+ */
+
+enum m0_btree_opcode;
+struct m0_btree_oimpl;
+
+struct m0_btree_op {
+	struct m0_sm_op        bo_op;
+	enum m0_btree_opcode   bo_opc;
+	struct m0_btree       *bo_arbor;
+	struct m0_btree_key    bo_key;
+	struct m0_btree_cb     bo_cb;
+	struct m0_be_tx       *bo_tx;
+	uint64_t               bo_flags;
+	struct m0_btree_oimpl *bo_i;
+};
+
+
+/** @} end of btree group */
+#endif /* __MOTR_BTREE_INTERNAL_H__ */
+
+/*
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
+/*
+ * vim: tabstop=8 shiftwidth=8 noexpandtab textwidth=80 nowrap
+ */
