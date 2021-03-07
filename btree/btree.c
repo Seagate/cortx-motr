26d9e4b060dc55224f143b564d9d23557133108b
Nikita Danilov <nikita.danilov@seagate.com>
2021-03-07 23:22:35 +0300

    btree: update DLD.
     - add Glossary,
    
     - add description of lookup.

--- btree/btree.c
+++ btree/btree.c
@@ -22,11 +22,186 @@
 /**
  * @addtogroup btree
  *
+ * Overview
+ * --------
+ *
+ * Glossary
+ * --------
+ *
+ * Btree documentation and implementation use the following terms.
+ *
+ * - segment, segment device, log device: segment is an area of motr process
+ *   address space in which meta-data are memory mapped. motr meta-data beck-end
+ *   (BE) retrieves meta-data from and stores them to a segment device. To
+ *   achieve transactional meta-data updates, meta-data are also logged to a log
+ *   device.
+ *
+ * - btree is a persistent container of key-value records, implemented by this
+ *   module. Key-value records and additional internal btree data are stored in
+ *   a segment. When a btree is actively used, some additional state is kept in
+ *   memory outside of the segment. A btree is an instance of btree type, which
+ *   specifies certain operational parameters.
+ *
+ *   btree persistent state is stored as a tree descriptor and a collection of
+ *   btree nodes. The descriptor and nodes are allocated within a segment.
+ *
+ * - btree descriptor is a persistent data structure, describing the tree. The
+ *   address of a tree is the address of its descriptor. The descriptor is
+ *   read-only after the tree is created.
+ *
+ * - btree node is a contiguous region of a segment allocated to hold tree
+ *   state. The nodes of a tree can have different size subject to tree type
+ *   constraints. There are 2 types of nodes:
+ *
+ *       -# internal nodes contain delimiting keys and pointers to child nodes;
+ *
+ *       -# leaf nodes contain key-value records.
+ *
+ *   A tree always has at least a root node. The root node can be either leaf
+ *   (if the tree is small) or internal. The root node is located right after
+ *   the tree descriptor. All other nodes are allocated and freed dynamically.
+ *
+ * - tree structure. An internal node has a set of children. A descendant of a
+ *   node is either its child or a descendant of a child. The parent of a node
+ *   is the (only) node (necessarily internal) of which the node is a child. An
+ *   ancestor of a node is either its parent of the parent of an ancestor. The
+ *   sub-tree rooted at a node is the node together with all its descendants.
+ *
+ *   A node has a level, assigned when the node is allocated. Leaves are on the
+ *   level 0 and the level of an internal node is one larger than the
+ *   (identical) level of its children. In other words, the tree is balanced:
+ *   the path from the root to any leaf has the same length;
+ *
+ * - delimiting key is a key separating ("delimiting") two children of an
+ *   internal node. E.g., in the diagram below, key[0] of the root node is the
+ *   delimiting key for child[0] and child[1]. btree algorithms guarantee that
+ *   any key in the sub-tree rooted an a child is less than the delimiting key
+ *   between this child and the next one, and not less than the delimiting key
+ *   between this child and the previous one;
+ *
+ * - node split ...
+ *
+ * - adding new root ...
+ *
+ * - tree traversal is a process of descending through the tree from the root to
+ *   the target leaf. Tree traversal takes a key as an input and returns the
+ *   leaf node that contains the given key (or should contain it, if the key is
+ *   missing from the tree). Such a leaf is unique by btree construction. All
+ *   tree operations (lookup, insertion, deletion) start with tree traversal.
+ *
+ *   Traversal starts with the root. By doing binary search over delimiting keys
+ *   in the root node, the target child, at which the sub-tree with the target
+ *   key is rooted, is found. The child is loaded in memory, if necessary, and
+ *   the process continues until a leaf is reached.
+ *
+ *
+ * Functional specification
+ * ------------------------
+ *
+ * Logical specification
+ * ---------------------
+ *
+ * Lookup
+ * ......
+ *
+ * Tree lookup (GET) operation traverses a tree to find a given key. If the key
+ * is found, the key and its value are the result of the operation. If the key
+ * is not present in the tree, the operation (depending on flags) either fails,
+ * or returns the next key (the smallest key in the tree greater than the
+ * missing key) and its value.
+ *
+ * Lookup takes a "cookie" as an additional optional parameter. A cookie
+ * (returned by a previous tree operation) is a safe pointer to the leaf node. A
+ * cookie can be tested to check whether it still points to a valid cached leaf
+ * node containing the target key. In the check is successful, the lookup
+ * completes.
+ *
+ * Otherwise, lookup performs a tree traversal.
+ *
+ * Insertion (PUT)
+ * ...............
+ *
+ * Deletion (DEL)
+ * ..............
+ *
+ * Iteration (NEXT)
+ * ................
+ *
+ * Data structures
+ * ---------------
+ *
+ * Concurrency
+ * -----------
+ *
+ * Persistent state
+ * ----------------
+ *
+ * @verbatim
+ *
+ * +------------+----------+----------+--------+----------+-----+----------+
+ * | tree descr | root hdr | child[0] | key[0] | child[1] | ... | child[N] |
+ * +------------+----------+----+-----+--------+----+-----+-----+----+-----+
+ *                              |                   |                |
+ *  <---------------------------+                   |                +-------->
+ *                                                  |
+ * +------------------------------------------------+
+ * |
+ * v
+ * +----------+----------+--------+----------+-----+----------+
+ * | node hdr | child[0] | key[0] | child[1] | ... | child[N] |
+ * +----------+----+-----+--------+----+-----+-----+----+-----+
+ *                 |                   |                |
+ *   <-------------+                   |                +---------->
+ *                                     |
+ * +-----------------------------------+
+ * |
+ * v
+ * +----------+----------+--------+----------+-----+----------+
+ * | node hdr | child[0] | key[0] | child[1] | ... | child[N] |
+ * +----------+----+-----+--------+----+-----+-----+----+-----+
+ *                 |                   |                |
+ *   <-------------+                   .                +---------->
+ *                                     .
+ *                                     .
+ * +-------------------- ...
+ * |
+ * v
+ * +----------+--------+--------+--------+--------+-----+--------+--------+
+ * | leaf hdr | key[0] | val[0] | key[1] | val[1] | ... | key[N] | val[N] |
+ * +----------+--------+--------+--------+--------+-----+--------+--------+
+ *
+ * @endverbatim
+ *
+ * (Source: https://asciiflow.com/#/share/eJyrVspLzE1VssorzcnRUcpJrEwtUrJSqo5RqohRsrIws9SJUaoEsozMDYGsktSKEiAnRunRlD3DFcXE5AFJhZKi1FSFlNTi5CIFELcoP79EISMFwknOyMxJiTaIBXOyUythTIi4IYSjp6eHJOgHFoSYPVwR0HcK%2BAAoNIgSHUFmPZq2ixZRQbQbiELTdhHyNo6gIF%2FbgCdmXCkchZw2eJ1KjEcU8vJTUkdsqYYlcT6irCQgWfuj6S3Eu5doowkgonMzdk9SQyX9Y%2FoRlmzb82hKA160hgIFZOglqGUC2NlNyNm2CTnbNiGybRNytm2CZtsm5GzbBDZsCiE7txBy4xbceomJHFKtm4E13zVhT2pYFBKtXQ%2BXZnCmxR9oCNcSZzChWCA6y%2BoB1UEQhVkGlGIgqY2UDEPvIpx6pUOTQk5qYho8U0HyUSwoIMoSc5ArRWg9CBJFqxJBsn4IWaS6kWAmG%2FhAICfclGqVagHq968y)
+ *
+ * Liveness and ownership
+ * ----------------------
+ *
+ * Use cases
+ * ---------
+ *
+ * Tests
+ * -----
+ *
+ * State machines
+ * --------------
+ *
+ * Sub-modules
+ * -----------
+ *
+ * Interfaces
+ * ----------
+ *
+ * Failures
+ * --------
+ *
+ * Compatibility
+ * -------------
+ *
  * @{
  */
 
-#include "lib/cookie.h"
-#define M0_TRACE_SUBSYSTEM M0_TRACE_SUBSYS_XXX
+#define M0_TRACE_SUBSYSTEM M0_TRACE_SUBSYS_BTREE
 #include "lib/trace.h"
 #include "lib/rwlock.h"
 #include "btree/btree.h"
@@ -57,7 +232,7 @@ enum base_phase {
 	P_INIT = M0_SOS_INIT,
 	P_DONE = M0_SOS_DONE,
 	P_DOWN = M0_SOS_NR,
-	P_SETUP
+	P_SETUP,
 	P_LOCK,
 	P_CHECK,
 	P_ACT,
@@ -69,9 +244,9 @@ enum base_phase {
 static int get_tick(struct m0_btree_op *bop) {
 	switch (bop->bo_op.o_sm.s_state) {
 	case P_INIT:
-		if (!m0_cookie_is_null(&bop->bo_key.k_cookie))
-			return pg_op_init(&bop->bo_op,
-					  &bop->bo_i->i_pop, P_TRYCOOKIE);
+		if (!m0_bcookie_is_null(&bop->bo_key.k_cookie))
+			return cookie_op(&bop->bo_op,
+					 &bop->bo_i->i_pop, P_TRYCOOKIE);
 		else
 			return P_SETUP;
 	case P_SETUP:
@@ -113,7 +288,6 @@ static int get_tick(struct m0_btree_op *bop) {
 }
 
 #undef M0_TRACE_SUBSYSTEM
-
 
 /*
  * Test plan:
