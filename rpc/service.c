f4ace99a603ed5939f082e8e5d7fcb6babc4eaac
Nikita Danilov <nikita.danilov@seagate.com>
2021-03-07 22:07:28 +0300

    rpc/service.c: update to M0_CF_ONESHOT flag.

--- rpc/service.c
+++ rpc/service.c
@@ -237,6 +237,7 @@ m0_rpc_service_reverse_session_put(struct m0_rpc_session *sess, bool disc)
 	rlk  = container_of(sess, struct m0_rpc_link, rlk_sess);
 	revc = container_of(rlk, struct m0_reverse_connection, rcf_rlink);
 
+	M0_SET0(&revc->rcf_disc_wait);
 	if (revc->rcf_rlink.rlk_connected &&
 	    !revc->rcf_rlink.rlk_async_disconnecting) {
 		revc->rcf_rlink.rlk_async_disconnecting = true;
@@ -244,6 +245,7 @@ m0_rpc_service_reverse_session_put(struct m0_rpc_session *sess, bool disc)
 		m0_clink_fini(&revc->rcf_disc_wait);
 		m0_clink_init(&revc->rcf_disc_wait,
 			      disc ? &rev_conn_disconnected_cb : NULL);
+		revc->rcf_disc_wait.cl_flags |= M0_CF_ONESHOT;
 		revc->rcf_disc_wait.cl_is_oneshot = true;
 		m0_rpc_link_disconnect_async(&revc->rcf_rlink,
 				m0_time_from_now(M0_REV_CONN_TIMEOUT, 0),
