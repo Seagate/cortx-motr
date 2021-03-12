3d502f3d7e83581dae2500d77cac3e534cf90b00
Sergey Shilov <sergey.shilov@seagate.com>
2021-03-12 08:12:28 -0700

    Fixes according to code review.
    Signed-off-by: Sergey Shilov <sergey.shilov@seagate.com>

--- motr/st/utils/ha_msg_send.sh
+++ motr/st/utils/ha_msg_send.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# set -x
+#set -ex
+
 motr_st_util_dir=$(dirname $(readlink -f $0))
 m0t1fs_dir="$motr_st_util_dir/../../../m0t1fs/linux_kernel/st"
 
@@ -19,17 +20,25 @@ proc_state_change()
     local fid=$2
     local state=$3
 
-    # Generate HA event
-    echo "Send HA event for motr"
-    echo "c_endpoint is : $c_endpoint"
-    echo "s_endpoint is : $s_endpoint"
-
     send_ha_events "$fid" "$state" "$s_endpoint" "$c_endpoint"
 }
 
-# {0x72| ((^r|1:14), ..., "192.168.122.122@tcp:12345:2:2", ...
-# {0x72| ((^r|1:10), ..., "192.168.122.122@tcp:12345:2:1", ...
+# {0x72| ((^r|1:12), ..., "192.168.122.122@tcp:12345:2:1", ...
+# {0x72| ((^r|1:26), ..., "192.168.122.122@tcp:12345:2:2", ...
 
-# EXAMPLE: ha_msg_send.sh "12345:2:2" "^r|1:26" "transient"
-# EXAMPLE: ha_msg_send.sh "12345:2:3" "^r|1:12" "online"
+function usage()
+{
+    echo "Usage:"
+    echo "$(basename "$0") <target_endpoint> <fid> <state>"
+    echo ""
+    echo "EXAMPLE: $(basename "$0") \"12345:2:2\" \"^r|1:26\" \"transient\""
+    echo "EXAMPLE: $(basename "$0") \"12345:2:3\" \"^r|1:12\" \"online\""
+}
+
+if [ $# -ne 3 ]
+then
+    usage
+    exit 1
+fi
+
 proc_state_change $1 $2 $3 > /dev/null
