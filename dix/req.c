0b4545d6d81dd373c859249ac85fbfc3cab14441
Andriy Tkachuk <andriy.tkachuk@seagate.com>
2021-03-19 01:33:24 +0000

    dix/req.c: fix compilation error with gcc-8.3
      dix/req.c: In function ‘dix_rop_completed’:
      dix/req.c:1647:15: error: ‘rop_del_phase2’ may be used uninitialized in this function [-Werror=maybe-uninitialized]
         req->dr_rop = rop_del_phase2;
         ~~~~~~~~~~~~^~~~~~~~~~~~~~~~
      cc1: all warnings being treated as errors
    
    Solution: init rop_del_phase2 with NULL.
    
    Signed-off-by: Andriy Tkachuk <andriy.tkachuk@seagate.com>

--- dix/req.c
+++ dix/req.c
@@ -1619,7 +1619,7 @@ static void dix_rop_completed(struct m0_sm_group *grp, struct m0_sm_ast *ast)
 {
 	struct m0_dix_req     *req = ast->sa_datum;
 	struct m0_dix_rop_ctx *rop = req->dr_rop;
-	struct m0_dix_rop_ctx *rop_del_phase2;
+	struct m0_dix_rop_ctx *rop_del_phase2 = NULL;
 	bool                   del_phase2 = false;
 	struct m0_dix_cas_rop *cas_rop;
 
