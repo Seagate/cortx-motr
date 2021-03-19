e9383ce0d05be52e43868729fe47dfa909e24e2c
RadhaGulhane13 <radha.gulhane@seagate.com>
2021-03-19 06:21:47 -0600

    Added state diagram of deletion and Iteration operation
    Signed-off-by: RadhaGulhane13 <radha.gulhane@seagate.com>

--- btree/btree.c
+++ btree/btree.c
@@ -233,8 +233,6 @@
  * node is still here) and there is enough free space in the leaf node to
  * complete insertion without going up through the tree.
  *
- * Deletion (DEL)
- * ..............
  *
  * Deletion (DEL)
  * ..............
