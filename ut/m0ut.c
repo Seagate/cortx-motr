c43dbc491c5cff012e365d040c221feb0a251d89
Shashank Parulekar <shashank.parulekar@seagate.com>
2021-03-31 10:03:29 +0530

    Added initial ut code in btree/btree.c
    Added comments to methods in node_type strcture used for btree.
    Added sleep to generate a different seed for btree ut.
    Added node_invariant in btree
    Added tree_get, tree_create and modified ff_find routine in new btree.
    Added changes to ut for testing btree_get and btree_put calls.
    Fixes to generic_move routine in btree.c
    
    Signed-off-by: Shashank Parulekar <Shashank.Parulekar@seagate.com>

--- ut/m0ut.c
+++ ut/m0ut.c
@@ -174,6 +174,7 @@ extern struct m0_ut_suite xcode_ff2c_ut;
 extern struct m0_ut_suite xcode_ut;
 extern struct m0_ut_suite sns_flock_ut;
 extern struct m0_ut_suite ut_suite_pi;
+extern struct m0_ut_suite btree_ut;
 
 static void tests_add(struct m0_ut_module *m)
 {
@@ -204,6 +205,7 @@ static void tests_add(struct m0_ut_module *m)
 	 */
 	m0_ut_add(m, &dtm0_ut, true);
 
+	m0_ut_add(m, &btree_ut, true);
 	m0_ut_add(m, &capa_ut, true);
 	m0_ut_add(m, &cas_client_ut, true);
 	m0_ut_add(m, &cas_service_ut, true);
