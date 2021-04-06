910d5ff9e0f03c2e98149453b61f663b53d7534a
Upendra Patwardhan <upendra.patwardhan@seagate.com>
2021-04-06 16:24:08 +0530

    EOS-18060, EOS-18078, EOS-18082 - Update buffer pool config and fix ST-42 and ST-43 for libfabric (#563)
    * Implementation of work request queues to support RDMA transfer of multiple segments.
    * Use separate buffer pool config for ping and bulk buffers.
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>

--- ioservice/io_foms.c
+++ ioservice/io_foms.c
@@ -1534,11 +1534,8 @@ static int zero_copy_initiate(struct m0_fom *fom)
 		used_size = rwfop->crw_desc.id_descs[fom_obj->
 						fcrw_curr_desc_index].bdd_used;
 
-		if (dom->nd_xprt == &m0_net_lnet_xprt) {
-			segs_nr = used_size / max_seg_size;
-		} else {
-			segs_nr = 1;
-		}
+		segs_nr = ((used_size / max_seg_size) + \
+			   ((used_size % max_seg_size) ? 1 : 0));
 
 		M0_LOG(M0_DEBUG, "segs_nr %d", segs_nr);
 
