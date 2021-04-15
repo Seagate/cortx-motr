39e51f0bb90289c0e294a5b0516835de6151add4
Papan Kumar Singh <papan.kumarsingh@seagate.com>
2021-04-14 23:36:42 -0600

    Renamed max_msgs_size to min_recv_size.
    Signed-off-by: Papan Kumar Singh <papan.kumarsingh@seagate.com>

--- rpc/rpc_machine.c
+++ rpc/rpc_machine.c
@@ -456,7 +456,7 @@ static int rpc_tm_setup(struct m0_net_transfer_mc *tm,
 {
 	struct m0_clink tmwait;
 	int             rc;
-	uint32_t max_msgs_size;
+	uint32_t        min_recv_size;
 
 	M0_ENTRY("tm: %p, net_dom: %p, ep_addr: %s", tm, net_dom,
 		 (char *)ep_addr);
@@ -471,12 +471,12 @@ static int rpc_tm_setup(struct m0_net_transfer_mc *tm,
 		return M0_ERR_INFO(rc, "TM initialization");
 
 	if (net_dom->nd_xprt == &m0_net_lnet_xprt)
-		max_msgs_size = m0_rpc_max_msg_size(net_dom, msg_size);
+		min_recv_size = m0_rpc_max_msg_size(net_dom, msg_size);
 	else
-		max_msgs_size = 1;
+		min_recv_size = 1;
 
 	rc = m0_net_tm_pool_attach(tm, pool, &rpc_buf_recv_cb,
-				   max_msgs_size,
+				   min_recv_size,
 				   m0_rpc_max_recv_msgs(net_dom, msg_size),
 				   qlen);
 	if (rc < 0) {
