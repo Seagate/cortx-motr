b67503e226b771c2deaeee06299e3be9e534eccd
VinothVijayakumar-dev <vinoth.v@seagate.com>
2021-04-16 09:59:59 +0530

    [EOS-18074] Adding libfab Transport for all the STs (#548)
    * [EOS-18074] Adding libfab Transport for all the STs
    	- update XPT variable to libfab, if libfab is available
    
    Signed-off-by: Vinoth.V <vinoth.v@seagate.com>

--- console/st/console-st.sh
+++ console/st/console-st.sh
@@ -44,6 +44,8 @@ CONF_PROFILE='<0x7000000000000001:0>'
 NODE_UUID=02e94b88-19ab-4166-b26b-91b51f22ad91  # required by `common.sh'
 . $M0_SRC_DIR/m0t1fs/linux_kernel/st/common.sh  # modload_galois
 
+XPRT=$(m0_default_xpt)
+
 start_server()
 {
 	modprobe lnet
@@ -64,7 +66,7 @@ start_server()
 	##
 	$M0_SRC_DIR/utils/mkfs/m0mkfs -T AD -D console_st_srv.db \
 	    -S console_st_srv.stob -A linuxstob:console_st_srv-addb.stob \
-	    -w 10 -e lnet:$SERVER_EP_ADDR -H $SERVER_EP_ADDR \
+	    -w 10 -e "$XPRT:$SERVER_EP_ADDR" -H $SERVER_EP_ADDR \
 	    -q 2 -m $((1 << 17)) \
 	    -c $CONF_FILE_PATH  \
 	    &>$SANDBOX_DIR/mkfs.log || die 'm0mkfs failed'
