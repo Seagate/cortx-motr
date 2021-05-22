b87a6935baa7b9df62a4c68b4a46d1f4d49295ba
Radha Gulhane <radha.gulhane@seagate.com>
2021-05-22 13:07:56 +0530

    Implemented btree_put_tick() function for insert operation of btree Signed-off-by: RadhaGulhane13 <radha.gulhane@seagate.com>

--- btree/btree.h
+++ btree/btree.h
@@ -132,9 +132,8 @@ void m0_btree_get(struct m0_btree *arbor, const struct m0_btree_key *key,
 		  const struct m0_btree_cb *cb, uint64_t flags,
 		  struct m0_btree_op *bop);
 void m0_btree_put(struct m0_btree *arbor, struct m0_be_tx *tx,
-		  const struct m0_btree_key *key,
-		  const struct m0_btree_cb *cb, uint64_t flags,
-		  struct m0_btree_op *bop);
+		  const struct m0_btree_rec *rec, const struct m0_btree_cb *cb,
+		  uint64_t flags, struct m0_btree_op *bop);
 void m0_btree_del(struct m0_btree *arbor, const struct m0_btree_key *key,
 		  const struct m0_btree_cb *cb, uint64_t flags,
 		  struct m0_btree_op *bop);
