4c742adebc31ecdbbae85ceffcb20bf728e15c43
Shipra Gupta <shipra.gupta@seagate.com>
2021-05-26 09:25:54 -0600

    EOS-20758: Implementation of API using current S3 implementation as reference includes MD5 - client side
    Problem:
    Do the implemnetation for motr client api,
    implementation include calculations for MD5 checksums and digest
    and the test code.
    Do changes rquired for code compilation.
    
    Solution:
    Done the implementation for calculating MD5checksums and digest
    Written code for UT.
    Integrated motr code with openssl MD5 library
    so that newly added code for data protection
    can compile.
    
    Please refer following PR for review comments
    https://github.com/gshipra/cortx-motr/pull/2/
    
    Testing
    Code compiled and newly written UT passed.
    
    Signed-off-by: Shipra Gupta <shipra.gupta@seagate.com>

--- motr/client_internal.h
+++ motr/client_internal.h
@@ -1,6 +1,6 @@
 /* -*- C -*- */
 /*
- * Copyright (c) 2016-2020 Seagate Technology LLC and/or its Affiliates
+ * Copyright (c) 2016-2021 Seagate Technology LLC and/or its Affiliates
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -946,9 +946,9 @@ M0_INTERNAL bool entity_id_is_valid(const struct m0_uint128 *id);
 /**
  * Calculate checksum/protection info for data/KV
  *
- * @param pi  pi struct m0_md5_inc_digest_pi
+ * @param pi  pi struct m0_md5_inc_context_pi
  *            This function will calculate the checksum and set
- *            pi_value field of struct m0_md5_inc_digest_pi.
+ *            pi_value field of struct m0_md5_inc_context_pi.
  * @param seed seed value (obj_id+data_unit_offset) required to calculate
  *             the checksum. If this pointer is NULL that means either
  *             this checksum calculation is meant for KV or user does
@@ -956,19 +956,19 @@ M0_INTERNAL bool entity_id_is_valid(const struct m0_uint128 *id);
  * @param m0_bufvec - Set of buffers for which checksum is computed.
  * @param flag if flag is M0_PI_CALC_UNIT_ZERO, it means this api is called for
  *             first data unit and MD5_Init should be invoked.
- * @param[out] curr_digest digest of data unit N, will be required to calculate checksum for
- *                         next data unit, N+1. Curre_digest is calculated and set in this func.
+ * @param[out] curr_context context of data unit N, will be required to calculate checksum for
+ *                         next data unit, N+1. Curre_context is calculated and set in this func.
  * @param[out] pi_value_without_seed - Caller may need checksum value without seed and with seed.
  *                                     With seed checksum is set in pi_value of PI type struct.
  *                                     Without seed checksum is set in this field.
  */
 
-M0_INTERNAL int m0_calculate_md5_inc_digest(
-		struct m0_md5_inc_digest_pi *pi,
+M0_INTERNAL int m0_calculate_md5_inc_context(
+		struct m0_md5_inc_context_pi *pi,
 		struct m0_pi_seed *seed,
 		struct m0_bufvec *bvec,
 		enum m0_pi_calc_flag flag,
-		unsigned char *curr_digest,
+		unsigned char *curr_context,
 		unsigned char *pi_value_without_seed);
 
 
