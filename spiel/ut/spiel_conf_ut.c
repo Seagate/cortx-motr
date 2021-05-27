440e17e5508c2de8f3c3a0cff928a3597a1bffee
Abhishek Saha <abhishek.saha@seagate.com>
2021-05-27 19:51:12 +0530

    EOS-15988 conf: Remap 'Node' to 'Enclosure' (#356)
    This patch removes the association of 'Node' from 'Controller' and
    adds that to 'Enclosure' in the failure domain so that the controller
    can be treated as Disk-groups and can be marked a failed without marking
    the entire node as failed.
    
    Signed-off-by: Abhishek Saha <abhishek.saha@seagate.com>

--- spiel/ut/spiel_conf_ut.c
+++ spiel/ut/spiel_conf_ut.c
@@ -281,14 +281,6 @@ static void spiel_conf_create_conf_with_opt(struct m0_spiel    *spiel,
 			           &spiel_obj_fid[SPIEL_UT_OBJ_SITE]);
 	M0_UT_ASSERT(rc == 0);
 
-	rc = m0_spiel_enclosure_add(tx, &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE],
-				        &spiel_obj_fid[SPIEL_UT_OBJ_RACK]);
-	M0_UT_ASSERT(rc == 0);
-
-	rc = m0_spiel_enclosure_add(tx, &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE2],
-				        &spiel_obj_fid[SPIEL_UT_OBJ_RACK2]);
-	M0_UT_ASSERT(rc == 0);
-
 	rc = m0_spiel_node_add(tx,
 			       &spiel_obj_fid[SPIEL_UT_OBJ_NODE],
 			       256,
@@ -304,16 +296,25 @@ static void spiel_conf_create_conf_with_opt(struct m0_spiel    *spiel,
 			       10,
 			       0xff00ff00);
 	M0_UT_ASSERT(rc == 0);
+
+	rc = m0_spiel_enclosure_add(tx, &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE],
+				        &spiel_obj_fid[SPIEL_UT_OBJ_RACK],
+				        &spiel_obj_fid[SPIEL_UT_OBJ_NODE]);
+	M0_UT_ASSERT(rc == 0);
+
+	rc = m0_spiel_enclosure_add(tx, &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE2],
+				        &spiel_obj_fid[SPIEL_UT_OBJ_RACK2],
+				        &spiel_obj_fid[SPIEL_UT_OBJ_NODE2]);
+	M0_UT_ASSERT(rc == 0);
+
 	rc = m0_spiel_controller_add(tx,
 				     &spiel_obj_fid[SPIEL_UT_OBJ_CONTROLLER],
-				     &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE],
-				     &spiel_obj_fid[SPIEL_UT_OBJ_NODE]);
+				     &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE]);
 	M0_UT_ASSERT(rc == 0);
 
 	rc = m0_spiel_controller_add(tx,
 				     &spiel_obj_fid[SPIEL_UT_OBJ_CONTROLLER2],
-				     &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE2],
-				     &spiel_obj_fid[SPIEL_UT_OBJ_NODE2]);
+				     &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE2]);
 	M0_UT_ASSERT(rc == 0);
 
 	rc = m0_spiel_drive_add(tx,
@@ -659,11 +660,6 @@ static void spiel_conf_create_invalid_configuration(struct m0_spiel    *spiel,
 			       &spiel_obj_fid[SPIEL_UT_OBJ_SITE]);
 	M0_UT_ASSERT(rc == 0);
 
-	rc = m0_spiel_enclosure_add(tx,
-			FID_MOVE(spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE], 5),
-			&spiel_obj_fid[SPIEL_UT_OBJ_RACK]);
-	M0_UT_ASSERT(rc == 0);
-
 	rc = m0_spiel_node_add(tx,
 			       FID_MOVE(spiel_obj_fid[SPIEL_UT_OBJ_NODE], 6),
 			       256,
@@ -672,10 +668,15 @@ static void spiel_conf_create_invalid_configuration(struct m0_spiel    *spiel,
 			       0xff00ff00);
 	M0_UT_ASSERT(rc == 0);
 
+	rc = m0_spiel_enclosure_add(tx,
+			FID_MOVE(spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE], 5),
+			&spiel_obj_fid[SPIEL_UT_OBJ_RACK],
+			&spiel_obj_fid[SPIEL_UT_OBJ_NODE]);
+	M0_UT_ASSERT(rc == 0);
+
 	rc = m0_spiel_controller_add(tx,
 				     FID_MOVE(spiel_obj_fid[SPIEL_UT_OBJ_CONTROLLER], 7),
-				     &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE],
-				     &spiel_obj_fid[SPIEL_UT_OBJ_NODE]);
+				     &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE]);
 	M0_UT_ASSERT(rc == 0);
 
 	rc = m0_spiel_drive_add(tx,
@@ -1151,22 +1152,6 @@ static void spiel_conf_create_fail(void)
 			            &spiel_obj_fid[SPIEL_UT_OBJ_SITE]);
 	M0_UT_ASSERT(rc == 0);
 
-	/* Enclosure */
-	rc = m0_spiel_enclosure_add(&tx,
-				    &fake_fid,
-				    &spiel_obj_fid[SPIEL_UT_OBJ_RACK]);
-	M0_UT_ASSERT(rc == -EINVAL);
-
-	rc = m0_spiel_enclosure_add(&tx,
-				    &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE],
-				    &fake_fid);
-	M0_UT_ASSERT(rc == -EINVAL);
-
-	rc = m0_spiel_enclosure_add(&tx,
-				    &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE],
-				    &spiel_obj_fid[SPIEL_UT_OBJ_RACK]);
-	M0_UT_ASSERT(rc == 0);
-
 	/* Node */
 	rc = m0_spiel_node_add(&tx,
 			       &fake_fid,
@@ -1178,29 +1163,45 @@ static void spiel_conf_create_fail(void)
 			       256, 2, 10, 0xff00ff00);
 	M0_UT_ASSERT(rc == 0);
 
+	/* Enclosure */
+	rc = m0_spiel_enclosure_add(&tx,
+				    &fake_fid,
+				    &spiel_obj_fid[SPIEL_UT_OBJ_RACK],
+				    &spiel_obj_fid[SPIEL_UT_OBJ_NODE]);
+	M0_UT_ASSERT(rc == -EINVAL);
+
+	rc = m0_spiel_enclosure_add(&tx,
+				    &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE],
+				    &fake_fid,
+				    &spiel_obj_fid[SPIEL_UT_OBJ_NODE]);
+	M0_UT_ASSERT(rc == -EINVAL);
+
+	rc = m0_spiel_enclosure_add(&tx,
+				    &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE],
+				    &spiel_obj_fid[SPIEL_UT_OBJ_RACK],
+				    &fake_fid);
+	M0_UT_ASSERT(rc == -EINVAL);
+
+	rc = m0_spiel_enclosure_add(&tx,
+				    &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE],
+				    &spiel_obj_fid[SPIEL_UT_OBJ_RACK],
+				    &spiel_obj_fid[SPIEL_UT_OBJ_NODE]);
+	M0_UT_ASSERT(rc == 0);
+
 	/* Controller */
 	rc = m0_spiel_controller_add(&tx,
+				     &fake_fid,
+				     &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE]);
+	M0_UT_ASSERT(rc == -EINVAL);
+
+	rc = m0_spiel_controller_add(&tx,
 				     &spiel_obj_fid[SPIEL_UT_OBJ_CONTROLLER],
-				     &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE],
 				     &fake_fid);
 	M0_UT_ASSERT(rc == -EINVAL);
 
 	rc = m0_spiel_controller_add(&tx,
-				     &fake_fid,
-				     &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE],
-				     &spiel_obj_fid[SPIEL_UT_OBJ_NODE]);
-	M0_UT_ASSERT(rc == -EINVAL);
-
-	rc = m0_spiel_controller_add(&tx,
 				     &spiel_obj_fid[SPIEL_UT_OBJ_CONTROLLER],
-				     &fake_fid,
-				     &spiel_obj_fid[SPIEL_UT_OBJ_NODE]);
-	M0_UT_ASSERT(rc == -EINVAL);
-
-	rc = m0_spiel_controller_add(&tx,
-				     &spiel_obj_fid[SPIEL_UT_OBJ_CONTROLLER],
-				     &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE],
-				     &spiel_obj_fid[SPIEL_UT_OBJ_NODE]);
+				     &spiel_obj_fid[SPIEL_UT_OBJ_ENCLOSURE]);
 	M0_UT_ASSERT(rc == 0);
 
 	/* drive */
@@ -1546,14 +1547,14 @@ static void spiel_conf_file_create_tree(struct m0_spiel_tx *tx)
 	rc = m0_spiel_rack_add(tx, &fid_rack, &fid_site);
 	M0_UT_ASSERT(rc == 0);
 
-	rc = m0_spiel_enclosure_add(tx, &fid_enclosure, &fid_rack);
-	M0_UT_ASSERT(rc == 0);
-
 	rc = m0_spiel_node_add(tx, &fid_node, 16000, 2, 3, 2);
 	M0_UT_ASSERT(rc == 0);
 
-	rc = m0_spiel_controller_add(tx, &fid_controller, &fid_enclosure,
-				     &fid_node);
+	rc = m0_spiel_enclosure_add(tx, &fid_enclosure, &fid_rack,
+				   &fid_node);
+	M0_UT_ASSERT(rc == 0);
+
+	rc = m0_spiel_controller_add(tx, &fid_controller, &fid_enclosure);
 	M0_UT_ASSERT(rc == 0);
 
 	rc = m0_spiel_drive_add(tx, &fid_drive1, &fid_controller);
