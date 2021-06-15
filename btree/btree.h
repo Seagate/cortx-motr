ac41446106f588c47fc0fd19d796c574a34ac003
Yash Bhamare <yash.bhamare@seagate.com>
2021-06-15 19:08:23 +0530

    EOS-20305: Implement code for m0_btree_open() (#747)
    *Add code for m0_btree_open()
    *Changes in sm/sm.c for proper working of return code
    *Changes to basic_tree_op UT as per requirement
    
    Signed-off-by: Yash Bhamare <yash.bhamare@seagate.com>

--- btree/btree.h
+++ btree/btree.h
@@ -95,6 +95,7 @@ struct m0_btree_cb {
  */
 struct m0_btree_idata {
 	void                       *addr;
+	struct m0_btree            *tree;
 	int                         num_bytes;
 	const struct m0_btree_type *bt;
 	const struct node_type     *nt;
@@ -142,11 +143,12 @@ enum m0_btree_opflag {
 	M0_BOF_UNIQUE = 1 << 0
 };
 
-int  m0_btree_open(void *addr, int nob, struct m0_btree **out);
+int  m0_btree_open(void *addr, int nob, struct m0_btree **out,
+		   struct m0_btree_op *bop);
 void m0_btree_close(struct m0_btree *arbor);
 void m0_btree_create(void *addr, int nob, const struct m0_btree_type *bt,
-		     const struct node_type *nt, struct m0_be_tx *tx,
-		     struct m0_btree_op *bop);
+		     const struct node_type *nt, struct m0_btree_op *bop,
+		     struct m0_be_tx *tx);
 void m0_btree_destroy(struct m0_btree *arbor, struct m0_btree_op *bop);
 void m0_btree_get(struct m0_btree *arbor, const struct m0_btree_key *key,
 		  const struct m0_btree_cb *cb, uint64_t flags,
