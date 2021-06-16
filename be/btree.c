Seagate/cortx-motr

commit fb19a784b900a73dfdb9fec45659b6c8b282644b

EOS-15302: limit value decremented without call to btree_pair_release() (#246)
* EOS-15302: limit value decremented without call to btree_pair_release()

fix added in EOS-6993 to reslove panic in btree_pair_release
is partial fix. In btree_truncate() when parent exist we
release parent->bt_num_active_key-1  key-val pair and
decrement limit if limit > 0, decrement of limit performaed
when parent parent->bt_num_active_key count become 0.
limit value has to be decremented when key-val pair released.

fix to decrement limit when key-val pair released.

Signed-off-by: Vikram Jadhav <vikram.jadhav@seagate.com>

be/btree.c
@@ -1256,16 +1256,17 @@ static void btree_truncate(struct m0_be_btree *btree, struct m0_be_tx *tx,
 			 * node->bt_child_arr[node->bt_num_active_key]
 			 * leaf node
 			 */
-			if (i > 0)
+			if (i > 0 && limit > 0) {
 				btree_pair_release(btree, tx,
 						&parent->bt_kv_arr[i-1]);
 
-			if (limit > 0)
 				limit--;
+				parent->bt_num_active_key--;
+			}
+
 			if (i == 0)
 				parent->bt_isleaf = true;
-			else
-				parent->bt_num_active_key--;
+
 			if (parent == btree->bb_root &&
 			    parent->bt_num_active_key == 0) {
 				/*

commit b64b1bba78571693a3123b462402736719632b1d

Eos 21067 Reverting change from commit id fb19a7 which caused btree truncate failures in some cases. (#751)
* Revert "EOS-15302: limit value decremented without call to btree_pair_release() (#246)"

This reverts commit fb19a784b900a73dfdb9fec45659b6c8b282644b which
caused some of the tests to fail while doing truncate operations.

* ut changes for testing corner cases for btree_truncate

Changed the ut be-ut:btree-create_truncate to test corner condition when
the limit and the bt_num_active_kv both reach 0 for a non-leaf node. The
handling of this case was not tested in the ut code before.

* Changes in m0beck to support debugging.

Added changes in beck.c to support mmapping the BE segment file to the memory
location where this BE segment is normally mmaped while cluster is running.
This will help during debugging the crash dump files since the BE segment
information is not embedded in the crash files.

Signed-off-by: Shashank Parulekar <Shashank.Parulekar@seagate.com>

be/btree.c
@@ -1205,7 +1205,7 @@ static void btree_truncate(struct m0_be_btree *btree, struct m0_be_tx *tx,
 {
 	struct m0_be_bnode *node;
 	struct m0_be_bnode *parent;
-	int                 i;
+	unsigned int        i;
 
 	/* Add one more reserve for non-leaf node. */
 	if (limit > 1)
@@ -1256,17 +1256,16 @@ static void btree_truncate(struct m0_be_btree *btree, struct m0_be_tx *tx,
 			 * node->bt_child_arr[node->bt_num_active_key]
 			 * leaf node
 			 */
-			if (i > 0 && limit > 0) {
+			if (i > 0)
 				btree_pair_release(btree, tx,
 						&parent->bt_kv_arr[i-1]);
 
+			if (limit > 0)
 				limit--;
-				parent->bt_num_active_key--;
-			}
-
 			if (i == 0)
 				parent->bt_isleaf = true;
-
+			else
+				parent->bt_num_active_key--;
 			if (parent == btree->bb_root &&
 			    parent->bt_num_active_key == 0) {
 				/*

be/tool/beck.c
@@ -37,6 +37,8 @@
 #include <pthread.h>
 #include <signal.h>           /* signal() to register ctrl + C handler */
 #include <yaml.h>
+#include <sys/mman.h>         /* mmap() */
+#include <unistd.h>           /* getpid() */
 
 #define M0_TRACE_SUBSYSTEM M0_TRACE_SUBSYS_BE
 #include "lib/trace.h"
@@ -525,6 +527,7 @@ static bool  dry_run = false;
 static bool  disable_directio = false;
 static bool  signaled = false;
 static bool  resume_scan = false;
+static bool  mmap_be_segment = false;
 
 static const char *offset_file = NULL;
 
@@ -617,7 +620,9 @@ int main(int argc, char **argv)
 		   M0_STRINGARG('d', "segment stob domain path path",
 			LAMBDA(void, (const char *s) {
 				beck_builder.b_dom_path = s;
-			})));
+			})),
+		   M0_FLAGARG('m', "MMAP BE segment file. Useful for "
+			      "developer debugging.", &mmap_be_segment));
 	if (result != 0)
 		errx(EX_USAGE, "Wrong option: %d.", result);
 	if (ut) {
@@ -628,6 +633,10 @@ int main(int argc, char **argv)
 		m0_build_info_print();
 		return EX_OK;
 	}
+
+	if (mmap_be_segment)
+		dry_run = true; /* Force dry run mode when asked to mmap. */
+
 	if (dry_run)
 		printf("Running in read-only mode.\n");
 
@@ -731,6 +740,29 @@ int main(int argc, char **argv)
 		seg_get(beck_scanner.s_file, &s_seg);
 		beck_scanner.s_seg = &s_seg;
 		beck_scanner.s_max_reg_size = DEFAULT_BE_MAX_TX_REG_SZ;
+
+		if (mmap_be_segment) {
+			void *ret;
+
+			errno = 0;
+			ret = mmap(s_seg.bs_addr, s_seg.bs_size,
+					PROT_READ|PROT_WRITE,
+					MAP_FIXED | MAP_PRIVATE | MAP_NORESERVE, 
+			     		fileno(beck_scanner.s_file),
+					s_seg.bs_offset);
+
+			if (ret == MAP_FAILED)
+				err(EXIT_FAILURE, "Failed to mmap memory. Error");
+
+			printf("BE segment file %s has been mmaped at "
+			       "address %p for %"PRId64" bytes.\n"
+			       "Please attach to process %u to browse BE "
+			       "segment data.\n", spath, s_seg.bs_addr, 
+						s_seg.bs_size, getpid());
+
+			while (1); /* Wait in endless loop for gdb connection */
+		}
+
 	}
 
 	result = scanner_cache_init(&beck_scanner);

be/ut/btree.c
@@ -610,6 +610,7 @@ static void truncate_tree(struct m0_be_btree *tree)
 	struct m0_buf           key;
 	char                    k[INSERT_KSIZE];
 	int                     rc;
+	int                     i;
 
 	M0_ENTRY();
 
@@ -627,8 +628,12 @@ static void truncate_tree(struct m0_be_btree *tree)
 	rc = m0_be_tx_open_sync(tx);
 	M0_UT_ASSERT(rc == 0);
 	btree_dbg_print(tree);
-	M0_BE_OP_SYNC_WITH(op, m0_be_btree_truncate(tree, tx,
-							op, INSERT_COUNT));
+	M0_ASSERT(INSERT_COUNT % 5 == 0);
+	for (i = 0; i < INSERT_COUNT; i+=5) {
+		M0_SET0(op);
+		M0_BE_OP_SYNC_WITH(op, m0_be_btree_truncate(tree, tx,
+							    op, 5));
+	}
 	M0_UT_ASSERT(m0_be_btree_is_empty(tree));
 	M0_BE_FREE_PTR_SYNC(tree, seg, tx);
 	m0_be_tx_close_sync(tx); /* Make things persistent. */