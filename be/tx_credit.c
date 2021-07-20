f6209562a2f5f9c06419d3e86674f9ad52cdeffb
Nikhil Kumar Birgade <nikhil.birgade@seagate.com>
2021-07-20 17:32:40 +0530

    EOS-22551: Build callback list in transaction. (#870)
    Signed-off-by: Nikhil Kumar Birgade <nikhil.birgade@seagate.com>

--- be/tx_credit.c
+++ be/tx_credit.c
@@ -46,6 +46,7 @@ M0_INTERNAL void m0_be_tx_credit_add(struct m0_be_tx_credit *c0,
 {
 	c0->tc_reg_nr   += c1->tc_reg_nr;
 	c0->tc_reg_size += c1->tc_reg_size;
+	c0->tc_cb_nr	+= c1->tc_cb_nr;
 	if (M0_DEBUG_BE_CREDITS) {
 		m0_forall(i, M0_BE_CU_NR,
 			  c0->tc_balance[i] += c1->tc_balance[i], true);
