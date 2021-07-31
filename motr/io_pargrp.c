766021b499e93460b2bcbefd93ebb7daa020d8a6
Yatin Mahajan <yatin.mahajan@seagate.com>
2021-07-31 07:22:46 -0600

    EOS-22959: Fixed some ST crashes dut to this change
    Some SNS ralated ST's were crashing due to this change, Fixed those now.
    
    Signed-off-by: Yatin Mahajan <yatin.mahajan@seagate.com>

--- motr/io_pargrp.c
+++ motr/io_pargrp.c
@@ -1459,7 +1459,15 @@ static int pargrp_iomap_pages_mark_as_failed(struct pargrp_iomap       *map,
 
 		if (row == row_nr)
 			continue;
-
+		if (type == M0_PUT_PARITY) {
+			/*
+			 * Setting parity buffer type to M0_PBUF_DIR, So that
+			 * it will free the buffer allocated for dgread path.
+			 * This buffer will be freed in
+			 * pargrp_iomap_fini() --> data_buf_dealloc_fini()
+			 */
+			ioo->ioo_pbuf_type = M0_PBUF_DIR;
+		}
 		for (row = 0; row < row_nr; ++row) {
 			if (bufs[row][col] == NULL) {
 				bufs[row][col] =
