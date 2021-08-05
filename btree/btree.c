9471a100ef920cdc34d3720112fe25f4c88d2ca9
Radha Gulhane <radha.gulhane@seagate.com>
2021-08-05 05:45:28 -0600

    changes related to 80 char limit exceed
    Signed-off-by: Radha Gulhane <radha.gulhane@seagate.com>

--- btree/btree.c
+++ btree/btree.c
@@ -6951,11 +6951,12 @@ static void ut_multi_stream_kv_oper(void)
 			rc = m0_be_tx_open_sync(tx);
 			M0_ASSERT(rc == 0);
 
-			rc = M0_BTREE_OP_SYNC_WITH_RC(&kv_op,
-						      m0_btree_del(tree,
-								   &del_key_in_tree,
-								   &ut_cb, 0,
-								   &kv_op, tx));
+			rc =
+			M0_BTREE_OP_SYNC_WITH_RC(&kv_op,
+						 m0_btree_del(tree,
+							      &del_key_in_tree,
+							      &ut_cb, 0,
+							      &kv_op, tx));
 			M0_ASSERT(rc == 0 && del_data.flags == M0_BSC_SUCCESS);
 			m0_be_tx_close_sync(tx);
 			m0_be_tx_fini(tx);
