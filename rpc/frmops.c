dd7fc831a18e03367e662eb1185f84953340c241
Nikita Danilov <nikita.danilov@seagate.com>
2021-08-19 22:00:47 +0300

    rpc: call ->rio_sent() unconditionally.
    rpc promises to call m0_rpc_item_ops::rio_sent() when item is placed on the
    wire, or when sending process completes.
    
    Rpc users depend on ->rio_sent() being called in case of error, for all
    types of items (requests, one-ways, replies):
    
       - rpc itself (conn_terminate_reply_sent_cb());
    
       - ha/link.c (ha_link_outgoing_item_sent()).
    
    Rpc fails to invoke ->rio_sent() in the following cases:
    
       - item was cancelled (item_sent()),
       - item sent failed and item was not one-way (m0_rpc_item_failed()).
    
    Fix all these places to invoke ->rio_sent().
    
    Signed-off-by: Nikita Danilov <nikita.danilov@seagate.com>

--- rpc/frmops.c
+++ rpc/frmops.c
@@ -496,6 +496,7 @@ static void item_sent(struct m0_rpc_item *item)
 		 */
 		M0_ASSERT(m0_rpc_item_is_request(item));
 		M0_ASSERT(item->ri_error == -ECANCELED);
+		m0_rpc_item_sent_invoke(item);
 		return;
 	}
 
@@ -510,8 +511,7 @@ static void item_sent(struct m0_rpc_item *item)
 
 	if (item->ri_nr_sent == 1) { /* not resent */
 		stats->rs_nr_sent_items_uniq++;
-		if (item->ri_ops != NULL && item->ri_ops->rio_sent != NULL)
-			item->ri_ops->rio_sent(item);
+		m0_rpc_item_sent_invoke(item);
 	} else if (item->ri_nr_sent == 2) {
 		/* item with ri_nr_sent >= 2 are counted as 1 in
 		   rs_nr_resent_items i.e. rs_nr_resent_items counts number
