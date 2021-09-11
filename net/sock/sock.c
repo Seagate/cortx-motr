8ddfb1daee4f6e4da48098d2be1b5044780bf72d
Nikita Danilov <nikita.danilov@seagate.com>
2021-09-12 00:31:36 +0300

    More of the same.

--- net/sock/sock.c
+++ net/sock/sock.c
@@ -3528,7 +3528,9 @@ static int pk_io(struct mover *m, struct sock *s, uint64_t flag,
 		rc = 0;
 	} else if (errno == EINTR) { /* Nothing was ioed, repeat. */
 		rc = 0;
-	} else
+	} else if (M0_IN(rc, (EPIPE, ECONNRESET)))
+		rc = M0_RC(-errno);
+	else
 		rc = M0_ERR(-errno);
 	/*
 	 * printf("%s -> %s, %p: flag: %" PRIx64 ", tgt: %" PRIu64 ", nob %" PRIu64 ","
