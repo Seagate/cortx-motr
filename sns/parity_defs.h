0c2fc6323c63135c4f0894d3cb3485d7d50b4c07
Sanjog Vikram Naik <sanjog.naik@seagate.com>
2021-10-04 19:05:23 +0530

    Update define for ISAL_ENCODE_ENABLED
    - Updated the way to define ISAL_ENCODE_ENABLED to avoid
      [-Wexpansion-to-defined] warning in some compilers in some cases.
    
    Signed-off-by: Sanjog Vikram Naik <sanjog.naik@seagate.com>

--- sns/parity_defs.h
+++ sns/parity_defs.h
@@ -26,7 +26,11 @@
 /**
  *  Define the condition here to use Intel ISA library.
  */
-#define ISAL_ENCODE_ENABLED	(!defined(__KERNEL__))
+#ifndef __KERNEL__
+#define ISAL_ENCODE_ENABLED 1
+#else
+#define ISAL_ENCODE_ENABLED 0
+#endif
 
 /* __MOTR_SNS_PARITY_DEFS_H__ */
 #endif
