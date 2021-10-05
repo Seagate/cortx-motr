bc154574555b188a7c4bfcc46d3ea231f82d2c5a
Yash Bhamare <yash.bhamare@seagate.com>
2021-10-05 11:50:55 +0530

    EOS-24987 : Removal of temporary code (#1105)
    * Initial Commit for removing time code
    * Changes to code as per review comments
    
    Signed-off-by: Yash Bhamare <yash.bhamare@seagate.com>

--- btree/btree.h
+++ btree/btree.h
@@ -463,7 +463,7 @@ M0_INTERNAL void m0_btree_cursor_kv_get(struct m0_btree_cursor *it,
 /**
  * Determines if tree contains zero record.
  */
-bool m0_btree_is_empty(struct m0_btree *btree);
+M0_INTERNAL bool m0_btree_is_empty(struct m0_btree *btree);
 
 void m0_btree_op_init(struct m0_btree_op *bop, enum m0_btree_opcode *opc,
 		      struct m0_btree *arbor,
