8eb0bb45da9b6485db80625adc87409a4909cc3a
Shashank Parulekar <shashank.parulekar@seagate.com>
2021-10-06 15:13:27 +0530

    Changes to m0_btree_destroy_credit() for accepting m0_btree_type parameter. (#1115)
    Current implementation of m0_btree_destroy_credit() used the (m0_btree *)
    parameter for getting the correct node-type needed to get the destroy credit
    count. In some cases this tree pointer is not available at the code location
    where the credit calculations are done. To cover these scenarioes the modified
    m0_btree_destroy_credit() also takes m0_btree_type parameter which is used to
    find out the node type needed for getting the correct credit count.
    
    Signed-off-by: Shashank Parulekar <Shashank.Parulekar@seagate.com>

--- cob/cob.c
+++ cob/cob.c
@@ -1112,12 +1112,21 @@ int m0_cob_domain_destroy(struct m0_cob_domain *dom,
 	seg = m0_be_domain_seg(bedom, dom);
 	m0_be_0type_del_credit(bedom, &m0_be_cob0, cdid_str, &cred);
 	M0_BE_FREE_CREDIT_PTR(dom, seg, &cred);
+<<<<<<< HEAD
 	m0_btree_destroy_credit(dom->cd_object_index,   &cred, 1);
 	m0_btree_destroy_credit(dom->cd_namespace,      &cred, 1);
 	m0_btree_destroy_credit(dom->cd_fileattr_basic, &cred, 1);
 	m0_btree_destroy_credit(dom->cd_fileattr_omg,   &cred, 1);
 	m0_btree_destroy_credit(dom->cd_fileattr_ea,    &cred, 1);
 	m0_btree_destroy_credit(dom->cd_bytecount,    &cred, 1);
+=======
+	m0_btree_destroy_credit(dom->cd_object_index,   NULL, &cred, 1);
+	m0_btree_destroy_credit(dom->cd_namespace,      NULL, &cred, 1);
+	m0_btree_destroy_credit(dom->cd_fileattr_basic, NULL, &cred, 1);
+	m0_btree_destroy_credit(dom->cd_fileattr_omg,   NULL, &cred, 1);
+	m0_btree_destroy_credit(dom->cd_fileattr_ea,    NULL, &cred, 1);
+	m0_btree_destroy_credit(dom->cd_bytecount,      NULL, &cred, 1);
+>>>>>>> Changes to m0_btree_destroy_credit() for accepting m0_btree_type parameter. (#1115)
 
 	M0_SET0(tx);
 	m0_be_tx_init(tx, 0, bedom, grp, NULL, NULL, NULL, NULL);
