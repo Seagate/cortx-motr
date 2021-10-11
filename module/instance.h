9597fd600c93bd4ff0d6a75a4de7a9119b596aed
Shashank Parulekar <shashank.parulekar@seagate.com>
2021-10-11 16:44:55 +0530

    Updated year in Copyright notice. (#1127)
    Updated Copyright notices to reflect 2021 in the files which were modified as a
    part of btree development.
    
    Signed-off-by: Shashank Parulekar <Shashank.Parulekar@seagate.com>

--- module/instance.h
+++ module/instance.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2020 Seagate Technology LLC and/or its Affiliates
+ * Copyright (c) 2014-2021 Seagate Technology LLC and/or its Affiliates
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
