1ffe40a2e1127650ea693e87a4ba12cbb667b099
Mehul Joshi <mehul.joshi@seagate.com>
2021-11-16 18:29:14 +0530

    Revert "rpc: call ->rio_sent() unconditionally. (#980)" (#1229)
    This reverts commit c5c0646e2569709ccb9166369cf62661acc6e2d6.

--- rpc/frmops.c
+++ rpc/frmops.c
@@ -502,7 +502,6 @@ static void item_sent(struct m0_rpc_item *item)
 		 */
 		M0_ASSERT(m0_rpc_item_is_request(item));
 		M0_ASSERT(item->ri_error == -ECANCELED);
-		m0_rpc_item_sent_invoke(item);
 		return;
 	}
 
@@ -517,7 +516,8 @@ static void item_sent(struct m0_rpc_item *item)
 
 	if (item->ri_nr_sent == 1) { /* not resent */
 		stats->rs_nr_sent_items_uniq++;
-		m0_rpc_item_sent_invoke(item);
+		if (item->ri_ops != NULL && item->ri_ops->rio_sent != NULL)
+			item->ri_ops->rio_sent(item);
 	} else if (item->ri_nr_sent == 2) {
 		/* item with ri_nr_sent >= 2 are counted as 1 in
 		   rs_nr_resent_items i.e. rs_nr_resent_items counts number
