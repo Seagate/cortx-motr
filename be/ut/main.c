34335d7154155b0edc5c0b987a59d0465738c515
Nikhil Kumar Birgade <nikhil.birgade@seagate.com>
2021-11-29 17:02:27 +0530

    EOS-25149: Aligned Chunk header implementation and btree integration. (#1240)
    Signed-off-by: Nikhil Kumar Birgade <nikhil.birgade@seagate.com>

--- be/ut/main.c
+++ be/ut/main.c
@@ -263,10 +263,7 @@ struct m0_ut_suite be_ut = {
 		{ "alloc-oom",               m0_be_ut_alloc_oom               },
 		{ "alloc-info",              m0_be_ut_alloc_info              },
 		{ "alloc-spare",             m0_be_ut_alloc_spare             },
-/**
- *  Uncomment once the related changes have been implemented.
- *              { "alloc-align",             m0_be_ut_alloc_align             },
- */
+                { "alloc-align",             m0_be_ut_alloc_align             },
 		{ "obj",                     m0_be_ut_obj_test                },
 		{ "actrec",                  m0_be_ut_actrec_test             },
 #endif /* __KERNEL__ */
