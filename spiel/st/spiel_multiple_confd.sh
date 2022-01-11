4bbdfcaf77386ae6c5dd6bb25ca8298625b986a4
Yeshpal Jain <yeshpal.jain@seagate.com>
2022-01-11 00:34:49 -0700

    EOS-6081: port m0spiel utility to python3
    Signed-off-by: Yeshpal Jain <yeshpal.jain@seagate.com>

--- spiel/st/spiel_multiple_confd.sh
+++ spiel/st/spiel_multiple_confd.sh
@@ -62,7 +62,7 @@ XPRT=$(m0_default_xprt)
 start() {
     # install "motr" Python module required by m0spiel tool
     cd $M0_SRC_DIR/utils/spiel
-    python2 setup.py install --record $INSTALLED_FILES > /dev/null ||
+    python3 setup.py install --record $INSTALLED_FILES > /dev/null ||
         die 'Cannot install Python "motr" module'
     sandbox_init
     _init
