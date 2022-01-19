714fe2fec2a250ccccfe6e91564c56dba7ff92cc
Madhavrao Vemuri <madhav.vemuri@seagate.com>
2022-01-19 09:15:08 -0700

    Fix codacy issues
    Signed-off-by: Madhavrao Vemuri <madhav.vemuri@seagate.com>

--- scripts/build-prep-1node-cortx-mgw.sh
+++ scripts/build-prep-1node-cortx-mgw.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Clone, build and prepare to start a single-node cluster with mgw.
+# It uses first avaialble disk of type /dev/sd*
 #
 # Usage: ./build-prep-1node-cortx-mgw.sh [-dev] [-pkg] [-mgs]
 #
@@ -14,41 +15,35 @@
 set -e -o pipefail
 #set -x
 
-dev_mode=
-pkg=
 mgs="--without-dashboard"
-
-[[ $1 == "-dev" ]] && dev_mode='yes'
-
-[[ $1 == "-pkg"  || $2 == "-pkg" ]] && pkg='yes'
 
 [[ $1 == "-mgs"  || $2 == "-mgs" || $3 == "-mgs" ]] && mgs=""
 
 # CPUS 8, 16G RAM, 2 disks of minimum 50 G each
 
 [[ -d cortx ]] || {
-  available_device=`lsblk --output NAME,FSTYPE -dsn | awk '$2 == "" {print $1}' | grep sd | head -n 1` 
-  mkfs.ext4 /dev/$available_device
+  available_device=$(lsblk --output NAME,FSTYPE -dsn | awk '$2 == "" {print $1}' | grep sd | head -n 1)
+  mkfs.ext4 "/dev/$available_device"
   mkdir -p ~/cortx
-  mount /dev/$available_device ~/cortx
+  mount "/dev/$available_device" ~/cortx
 }
 
 cd ~/cortx
 
 yum install wget -y
 wget https://raw.githubusercontent.com/Seagate/cortx-motr/main/scripts/build-prep-1node.sh
-sh +x ~/build-prep-1node.sh $1 $2
+sh +x ~/build-prep-1node.sh "$1" "$2"
 rm build-prep-1node.sh
 
 
-echo "=========================================\n"
-echo "CORTX build and intallation is completed.\n"
-echo "=========================================\n"
+echo "========================================="
+echo "CORTX build and intallation is completed."
+echo "========================================="
 
 
-echo "=========================================\n"
-echo "Start Ceph RGW SAL with MOTR build .\n"
-echo "=========================================\n"
+echo "========================================="
+echo "Start Ceph RGW SAL with MOTR build ."
+echo "========================================="
 
 
 [[ -d ceph ]] || {
@@ -61,23 +56,26 @@ cd -
 cd ~/cortx/cortx-rgw
 cmake3 -GNinja -DWITH_PYTHON3=3.6 -DWITH_RADOSGW_MOTR=YES -B build
 cd build && time ninja vstart-base && time ninja radosgw-admin && time ninja radosgw
- 
-MDS=0 RGW=1 ../src/vstart.sh -d -n $mgs
-MDS=0 RGW=1 ../src/vstart.sh -d $mgs
 
-echo "=========================================\n"
-echo "Ceph RGW SAL with MOTR build is completed.\n"
-echo "=========================================\n"
+echo "Start Ceph RGW.." 
+MDS=0 RGW=1 ../src/vstart.sh -d -n "$mgs"
+../src/stop.sh
+
+
+echo "========================================="
+echo "Ceph RGW SAL with MOTR build is completed."
+echo "========================================="
 
 echo
 echo 'update singlenode.yaml with devices in "path: /dev/loop0" format'
-echo 'Don't use /dev/$available_device in cluster yaml'
-echo 'Don't use the devices with filesystem on them'
+echo 'Do not use "/dev/$available_device" in cluster yaml'
+echo 'Also do not use the devices with filesystem on them'
 echo 'Now you are ready to start the singlenode Motr cluster!'
 echo 'To start, run: hctl bootstrap --mkfs singlenode.yaml'
 echo 'To check:     hctl status'
 echo 'To shutdown:  hctl shutdown'
 echo "Follow the link for further steps"
 echo "https://seagate-systems.atlassian.net/wiki/spaces/PRIVATECOR/pages/808583540/Motr+RGW+MGW+on+single-node+HOWTO"
+echo 'run: hctl bootstrap --mkfs singlenode.yaml'
 echo "Update ceph.conf as mentioned in above link"
-echo "Then MDS=0 RGW=1 ../src/vstart.sh -d"
+echo "run:  MDS=0 RGW=1 ../src/vstart.sh -d $mgs"
