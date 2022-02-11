99e3fa65dca910dc87e1c5d2016afc47117ee540
Yeshpal Jain <yeshpal.jain@seagate.com>
2022-02-11 00:20:35 -0700

    Added nikita sugested code changes.
    Signed-off-by: Yeshpal Jain <yeshpal.jain@seagate.com>

--- sm/sm.c
+++ sm/sm.c
@@ -136,17 +136,16 @@ M0_INTERNAL void m0_sm_ast_post(struct m0_sm_group *grp, struct m0_sm_ast *ast)
 {
 	M0_PRE(ast->sa_cb != NULL);
 
-	/* allow concurrent calls */
-	if (!M0_ATOMIC64_CAS(&ast->sa_next, (struct m0_sm_ast*)NULL, ast))
+	if (!M0_ATOMIC64_CAS(&ast->sa_next,
+			     (struct m0_sm_ast *)NULL, grp->s_forkq))
 		return; /* already posted */
 
-	do {
+	while (1) {
+		if (M0_ATOMIC64_CAS(&grp->s_forkq, ast->sa_next, ast))
+			break;
 		ast->sa_next = grp->s_forkq;
-		/*
-		M0_LOG(M0_DEBUG, "grp=%p forkq_push: %p -> %p",
-		       grp, ast, ast->sa_next);
-		*/
-	} while (!M0_ATOMIC64_CAS(&grp->s_forkq, ast->sa_next, ast));
+	}
+
 	m0_clink_signal(&grp->s_clink);
 }
 
