94647ac6f3b9e553b0279799b079896a23446d85
Papan Kumar Singh <papan.kumarsingh@seagate.com>
2022-03-21 11:31:09 -0600

    removed codacy warning.
    Signed-off-by: Papan Kumar Singh <papan.kumarsingh@seagate.com>

--- scripts/install/opt/seagate/cortx/motr/libexec/core_logrotate.sh
+++ scripts/install/opt/seagate/cortx/motr/libexec/core_logrotate.sh
@@ -26,14 +26,13 @@
 source "/opt/seagate/cortx/motr/common/cortx_util_funcs.sh"
 
 usage() { 
-        echo "Usage: bash $(basename $0) [--help|-h]
-                     [-n CoreFileCount]
-Retain recent modified files of given count and remove older core files.
-where:
--n            number of latest core files to retain 
-              Physical : Default count of core files are 5+2 first files
-              virtual  : Default count of core files is 2
---help|-h     display this help and exit" 1>&2; 
+        echo "Usage: bash $(basename $0) [--help|-h] [-n CoreFileCount]"
+        "Retain recent modified files of given count and remove older core files."
+        "where:"
+        "-n   number of latest core files to retain" 
+              "Physical : Default count of core files are 5+2 first files"
+              "virtual  : Default count of core files is 2"
+        "--help|-h     display this help and exit" 1>&2; 
         exit 1; 
 }
 
@@ -60,12 +59,12 @@ core_files_max_count=5
 motr_coredirs=("/var/crash/" "/var/log/crash/")
 
 MACHINE_ID=$(cat /etc/machine-id)
-if [ -d /etc/cortx/motr/sysconfig/$MACHINE_ID ]; then
+if [ -d "/etc/cortx/motr/sysconfig/$MACHINE_ID" ]; then
     MOTR_SYSCONFIG="/etc/cortx/motr/sysconfig/$MACHINE_ID/motr"
 else
     MOTR_SYSCONFIG="/etc/sysconfig/motr"
 fi
-M0TR_M0D_CORE_DIR=$(source $MOTR_SYSCONFIG ; echo $MOTR_M0D_DATA_DIR)
+M0TR_M0D_CORE_DIR=$(source "$MOTR_SYSCONFIG" ; echo "$MOTR_M0D_DATA_DIR")
 if [ -n "$M0TR_M0D_CORE_DIR" ]; then
     motr_coredirs+=("$M0TR_M0D_CORE_DIR")
 fi
@@ -85,9 +84,10 @@ while getopts ":n:" option; do
 done
 
 if [[ $platform = "virtual" ]]; then
+
     core_files_max_count=2
 else
-    core_files_max_count=`expr $core_files_max_count + 2`
+    core_files_max_count=$(($core_files_max_count + 2))
 fi
 
 echo "Max core file count: $core_files_max_count"
@@ -103,27 +103,30 @@ for motr_coredir in ${motr_coredirs[@]}; do
     [[ $(check_param $motr_coredir) = "continue" ]] && continue || echo "$motr_coredir"
 
     # get the core directory of each m0d instance
-    core_dirs=`find $motr_coredir -maxdepth 1 -type d -name m0d-\*`
+    core_dirs=$(find $motr_coredir -maxdepth 1 -type d -name m0d-\*)
 
-    [[ $(check_param $core_dirs) = "continue" ]] && continue || echo "$core_dirs"
+    [[ $(check_param "$core_dirs") = "continue" ]] && continue || echo "$core_dirs"
 
     # iterate through all core directories of each m0d instance
     for core_dir in $core_dirs ; do
         # get the no. of core file count
-        core_files=`find $core_dir -maxdepth 1 -type f -name "core.*"`
-        core_files_count=`echo "$core_files" | grep -v "^$" | wc -l`
+        core_files=$(find $core_dir -maxdepth 1 -type f -name "core.*")
+        core_files_count=$(echo "$core_files" | grep -v "^$" | wc -l)
+
         echo "## found $core_files_count file(s) in core directory $core_dir ##"
+
         # check core files count is greater than max core file count
-        if [[ $core_files_count -gt $core_files_max_count ]]; then
+        if [[ "$core_files_coun"t -gt "$core_files_max_count" ]]; then
             # get files sort by date - older will come on top
             remove_file_count=`expr $core_files_count - $core_files_max_count`
 
             echo "## ($remove_file_count) file(s) can be removed from \
-                           core directory($core_dir) ##"
+                           core directory($core_dir) ##"               
 
             # get the files sorted by time modified 
             # (most recently modified comes last), that 
             # is older files comes first
+            
             if [[ $platform = "physical" ]]; then
                 peserve_files=`expr $core_files_max_count - 2`
                 files_to_remove=`ls -tr "$core_dir" | grep core | \
