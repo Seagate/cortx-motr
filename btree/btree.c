e415470a85c2dfbe7dce2324783b6cb5c0a1a9dc
Yash Bhamare <yash.bhamare@seagate.com>
2022-03-22 14:22:54 +0530

    CORTX-29247: Support in motr config to set the watermarks for memory limits (#1548)
    * Support btree lrulist watermarks through motr sysconfig
    
    Signed-off-by: Yash Bhamare <yash.bhamare@seagate.com>

--- btree/btree.c
+++ btree/btree.c
@@ -1427,19 +1427,25 @@ enum lru_used_space_watermark{
  * Watermarks for BE space occupied by nodes in lru list.
  */
 /** LRU purging should not happen below low used space watermark. */
-int64_t lru_space_wm_low    = LUSW_LOW;
+static int64_t lru_space_wm_low;
 
 /**
  * An ongoing LRU purging can be stopped after reaching target used space
  * watermark.
  */
-int64_t lru_space_wm_target = LUSW_TARGET;
+static int64_t lru_space_wm_target;
 
 /**
  * LRU purging should be triggered if used space is above high used space
  * watermark.
  */
-int64_t lru_space_wm_high   = LUSW_HIGH;
+static int64_t lru_space_wm_high;
+
+/**
+ * LRU purging should be triggered if used space is above high used space
+ * watermark.
+ */
+static bool lrulist_trickle_release;
 
 M0_TL_DESCR_DEFINE(ndlist, "node descr list", static, struct nd,
 		   n_linkage, n_magic, M0_BTREE_ND_LIST_MAGIC,
@@ -1839,6 +1845,27 @@ M0_INTERNAL void m0_btree_mod_fini(void)
 {
 	m0_btree_glob_fini();
 	m0_free(mod_get());
+}
+
+M0_INTERNAL void m0_btree_lrulist_set_lru_config(int64_t slow_lru_mem_release,
+						 int64_t wm_low,
+						 int64_t wm_target,
+						 int64_t wm_high)
+{
+	lrulist_trickle_release = (slow_lru_mem_release != 0) ? true : false;
+
+	lru_space_wm_low = (wm_low == 0) ? LUSW_LOW : wm_low;
+	lru_space_wm_target = (wm_target == 0) ? LUSW_TARGET : wm_target;
+	lru_space_wm_high = (wm_high == 0) ? LUSW_HIGH : wm_high;
+
+	M0_ASSERT(lru_space_wm_high >= lru_space_wm_target &&
+		  lru_space_wm_target >= lru_space_wm_low);
+
+	M0_LOG(M0_INFO, "Btree LRU List Watermarks: Low - %"PRIi64" Mid - "
+	       "%"PRIi64" High - %"PRIi64" \n", lru_space_wm_low,
+	       lru_space_wm_target, lru_space_wm_high);
+	M0_LOG(M0_INFO, "Btree LRU List trickle release: %s \n",
+	       lrulist_trickle_release ? "true" : "false");
 }
 
 /**
