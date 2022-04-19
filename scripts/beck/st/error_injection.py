cf973b9c33201e019fe6aed78131c0fffa02e202
Gaurav Gaur <gaurav.gaur@seagate.com>
2022-04-19 23:03:21 +0530

    Resolved comments
    Signed-off-by: Gaurav Gaur <gaurav.gaur@seagate.com>

--- scripts/beck/st/error_injection.py
+++ scripts/beck/st/error_injection.py
@@ -192,11 +192,11 @@ def ReadMetadata(offset):
 
 
 def ReadCompleteRecord(offset):
-    '''
+    """function read complete record starting after header and until footer for record.
 
-    This function returns complete data with list format in 8 bytes chunks,
-    starting after header and until footer for record.
-    '''
+    :offset: 8-byte offset of data
+    :rtype: Return record data and footer offset
+    """
     curr_record = []
     while 1:
         footerFound, data=ReadMetadata(offset)
@@ -356,11 +356,12 @@ def Induce512kbError():
 
 
 def ConvertAdstob2Cob(stob_f_container, stob_f_key):
-    '''
+    """Method to cob related data
 
-    This function take stob_f_container, stob_f_key
-    and returns cob_f_cotainer, cob_f_key and device_id.
-    '''
+    :stob_f_container: 16-byte container
+    :stob_f_key: 16-byte key
+    :rtype: cob_f_cotainer, cob_f_key and device_id
+    """
     M0_FID_DEVICE_ID_OFFSET = 32
     M0_FID_DEVICE_ID_MASK = 72057589742960640
     M0_FID_TYPE_MASK = 72057594037927935
