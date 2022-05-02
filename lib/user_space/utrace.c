1616195acfb5ac2d3bcdd1720588e75398b332ce
Papan Kumar Singh <papan.kumarsingh@seagate.com>
2022-05-02 02:32:18 -0600

    CORTX-28925: Append timestamp/any unique value to m0trace to save traces between container/pods restart.
    Signed-off-by: Papan Kumar Singh <papan.kumarsingh@seagate.com>

--- lib/user_space/utrace.c
+++ lib/user_space/utrace.c
@@ -61,10 +61,14 @@ static size_t trace_buf_size = M0_TRACE_UBUF_SIZE;
 
 static int logbuf_map()
 {
-	struct m0_trace_area *trace_area;
-	size_t trace_area_size = M0_TRACE_BUF_HEADER_SIZE + trace_buf_size;
-	int    rc;
+	struct    m0_trace_area *trace_area;
+	size_t    trace_area_size = M0_TRACE_BUF_HEADER_SIZE + trace_buf_size;
+	int       rc;
+        m0_time_t stamp = m0_time_now();
+        time_t    ts    = m0_time_seconds(stamp);
+        struct tm tm;
 
+        localtime_r(&ts, &tm);
 	M0_PRE((trace_area_size % m0_pagesize_get()) == 0);
 
 	if (strlen(trace_file_path) == 0) {
@@ -80,7 +84,11 @@ static int logbuf_map()
 		int available_bytes = sizeof trace_file_path -
 				      strlen(trace_file_path);
 		rc = snprintf(trace_file_path + strlen(trace_file_path),
-			available_bytes, "m0trace.%u", m0_pid_cached);
+			available_bytes,
+                        "m0trace.%u.%04d-%02d-%02d-%02d:%02d:%02d.%09"PRIu64,
+                        m0_pid_cached, tm.tm_year + 1900,
+                        tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min,
+                        tm.tm_sec, m0_time_nanoseconds(stamp));
 		if (rc < 0) {
 			warn("failed to construct trace file path");
 			return rc;
