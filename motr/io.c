cb72b3c0cdde872315367e235ca24228d32b3bf7
Andriy Tkachuk <andriy.tkachuk@seagate.com>
2022-05-05 20:52:55 +0100

    client: use no hole mode by default
    Use no hole mode by default and rename M0_OOF_NOHOLE flag
    to M0_OOF_HOLE, which user should set to enable hole mode.
    This will help to avoid returning corrupted data to the
    user in case there are no units at the server side in
    parity groups (for some reason) by automatically enabling
    degraded read mode in such cases.
    
    Signed-off-by: Andriy Tkachuk <andriy.tkachuk@seagate.com>

--- motr/io.c
+++ motr/io.c
@@ -734,7 +734,7 @@ int m0_obj_op(struct m0_obj       *obj,
 		  opcode == M0_OC_FREE ? "free" : "");
 	M0_PRE(obj != NULL);
 	M0_PRE(op != NULL);
-	M0_PRE(ergo(opcode == M0_OC_READ, M0_IN(flags, (0, M0_OOF_NOHOLE))));
+	M0_PRE(ergo(opcode == M0_OC_READ, M0_IN(flags, (0, M0_OOF_HOLE))));
 	M0_PRE(ergo(opcode != M0_OC_READ, M0_IN(flags, (0, M0_OOF_SYNC))));
 	if (M0_FI_ENABLED("fail_op"))
 		return M0_ERR(-EINVAL);
