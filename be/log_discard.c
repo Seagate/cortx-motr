Seagate/cortx-motr

commit db9f19501f43f9dd5070b31c954c8431c6d7e7b6

EOS-25111 be/log_discard: avoid race between wait() and flush op (#1092)
m0_be_log_discard_flush() may finish waiting after io is done, but
before lds_discard_ast() is posted. It would lead to
be_log_discard_wait() to not to wait for the ast to be finished, which
leads to an assertion failure [1]. In this case traces look like [2] and
stack backtrace looks like [3].

This patch moves flush op to DONE at the end of the ast, which makes
flush() wait until the ast is actually executed.

The bug was discovered during EOS-25005 patch testing.

[1]

motr[24531]:  a3b0  FATAL  [lib/assert.c:50:m0_panic]  panic: (!ld->lds_sync_in_progress) at m0_be_log_discard_fini() (be/log_discard.c:143)  [git: 2.0.0-307-72-g0e307f7b-dirty] /w/var/m0ut/m0trace.24531
Motr panic: (!ld->lds_sync_in_progress) at m0_be_log_discard_fini() be/log_discard.c:143 (errno: 4) (last failed: none) [git: 2.0.0-307-72-g0e307f7b-dirty] pid: 24531  /w/var/m0ut/m0trace.24531

[2]

92998540  157294.289.381909  7f86d97f98e0  DEBUG   be/log_discard.c:288:be_log_discard_sync_done_cb    ld=0x7f86f709d8f8
92998541  157294.289.382245  7f86d97f98b0  CALL    be/op.c:154:m0_be_op_reset                          > op=0x7f86f709da40
...
92998549  157294.289.431085  7f86d97f9c40  DEBUG   sm/sm.c:158:m0_sm_asts_run                          grp=0x22d96b0 forkq_pop: 0x7f86f709de10 <- 0x7f86f6049da0
92998550  157294.289.432156  7f86d97f9c00  CALL    be/log_discard.c:256:be_log_discard_ast             > ld=0x7f86f709d8f8
92998551  157294.289.433624  7ffd2e9e10c0  FATAL   lib/assert.c:50:m0_panic                            panic: (!ld->lds_sync_in_progress) at m0_be_log_discard_fini() (be/log_discard.c:143)  [git: 2.0.0-307-37-gd35074f6-dirty] /w/var/m0ut/m0trace.22409
92998552  157294.289.435003  7f86d97f9ba0  CALL    be/log_discard.c:229:be_log_discard_item_discard    > ld=0x7f86f709d8f8 ldi=0x7f862039bf40

be/log_discard.c
@@ -175,6 +175,17 @@ static bool be_log_discard_is_locked(struct m0_be_log_discard *ld)
 	return m0_mutex_is_locked(&ld->lds_lock);
 }
 
+static void be_log_discard_flush_finished(struct m0_be_log_discard *ld)
+{
+	M0_PRE(be_log_discard_is_locked(ld));
+	M0_PRE(ld->lds_flush_op != NULL);
+	M0_PRE(ld_start_tlist_is_empty(&ld->lds_start_q));
+
+	m0_be_op_active(ld->lds_flush_op);
+	m0_be_op_done(ld->lds_flush_op);
+	ld->lds_flush_op = NULL;
+}
+
 static void be_log_discard_check_sync(struct m0_be_log_discard *ld,
                                       bool                      force)
 {
@@ -197,18 +208,10 @@ static void be_log_discard_check_sync(struct m0_be_log_discard *ld,
 				break;
 			ld->lds_sync_item = ldi;
 		} m0_tl_endfor;
-		if (ld->lds_sync_item == NULL && ld->lds_flush_op != NULL) {
-			M0_ASSERT(ld_start_tlist_is_empty(&ld->lds_start_q));
-			m0_be_op_active(ld->lds_flush_op);
-			m0_be_op_done(ld->lds_flush_op);
-		}
+		if (ld->lds_sync_item == NULL && ld->lds_flush_op != NULL)
+			be_log_discard_flush_finished(ld);
 		if (ld->lds_sync_item != NULL) {
 			ld->lds_sync_in_progress = true;
-			if (ld->lds_flush_op != NULL) {
-				m0_be_op_set_add(ld->lds_flush_op,
-						 &ld->lds_sync_op);
-				ld->lds_flush_op = NULL;
-			}
 			M0_LOG(M0_DEBUG, "ld=%p lds_sync_item=%p",
 			       ld, ld->lds_sync_item);
 			/* be_log_discard_sync_done_cb() locks ld */
@@ -274,6 +277,8 @@ static void be_log_discard_ast(struct m0_sm_group *grp,
 	be_log_discard_check_sync(ld, false);
 	ld->lds_discard_ast_posted = false;
 
+	if (ld->lds_flush_op != NULL)
+		be_log_discard_flush_finished(ld);
 	if (ld->lds_discard_waiting)
 		m0_semaphore_up(&ld->lds_discard_wait_sem);
 	be_log_discard_unlock(ld);

commit 88fc334f88082f4fedec4beb16a3c0bd3fd4a7ad

be/log_discard: sync again in sync completion ast during flush if needed
Possible sequence of events:

1. Sync is initiated in be_log_discard_check_sync() (by lds_sync_deadline
   or ldsc_items_threshold, for example);
2. One or more m0_be_log_discard_item is started;
3. All items from (2) are finished;
4. m0_be_log_discard_flush() is called, but be_log_discard_check_sync()
   doesn't initiate new sync because another one, started in (1), is
   still in progress (i.e. lds_sync_in_progress is true);
5. be_log_discard_ast() is called. ld->lds_flush_op != NULL because it
   was set in (4), but items from (2) and (3) are still in lds_start_q,
   because they were not there when lds_sync_item was saved during (1);
6. FATAL  [lib/assert.c:50:m0_panic]  panic: (ld_start_tlist_is_empty(&ld->lds_start_q)) at be_log_discard_flush_finished() (be/log_discard.c:182)

This patch makes (6) impossible by calling
be_log_discard_check_sync(..., true) in case if:
- be_log_discard_ast() is called after m0_be_log_discard_flush();
- lds_start_q is not empty in be_log_discard_ast().

The bug was introduced in "db9f1950 EOS-25111 be/log_discard: avoid race
between wait() and flush op (#1092)".

Signed-off-by: Maxim Medved <max.medved@seagate.com>

be/log_discard.c
@@ -270,6 +270,8 @@ static void be_log_discard_ast(struct m0_sm_group *grp,
 	} while (ldi != sync_item);
 
 	be_log_discard_lock(ld);
+	M0_ASSERT(ld->lds_sync_item != NULL);
+	M0_ASSERT(ld->lds_sync_in_progress);
 	ld->lds_sync_item = NULL;
 	ld->lds_sync_in_progress = false;
 	if (ld_start_tlist_is_empty(&ld->lds_start_q))
@@ -277,8 +279,13 @@ static void be_log_discard_ast(struct m0_sm_group *grp,
 	be_log_discard_check_sync(ld, false);
 	ld->lds_discard_ast_posted = false;
 
-	if (ld->lds_flush_op != NULL)
-		be_log_discard_flush_finished(ld);
+	if (ld->lds_flush_op != NULL) {
+		if (ld_start_tlist_is_empty(&ld->lds_start_q)) {
+			be_log_discard_flush_finished(ld);
+		} else {
+			be_log_discard_check_sync(ld, true);
+		}
+	}
 	if (ld->lds_discard_waiting)
 		m0_semaphore_up(&ld->lds_discard_wait_sem);
 	be_log_discard_unlock(ld);