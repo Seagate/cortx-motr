b543fe68e167037b39dbe675c34205d1ad7a7d69
Upendra Patwardhan <upendra.patwardhan@seagate.com>
2022-05-15 23:56:41 -0600

    Update cob.c to use indirect addressing for btree.
    Signed-off-by: Upendra Patwardhan <upendra.patwardhan@seagate.com>

--- cob/cob.c
+++ cob/cob.c
@@ -645,7 +645,7 @@ int m0_cob_domain_create_prepared(struct m0_cob_domain          **out,
 				      m0_btree_create(&dom->cd_fa_basic_node,
 						      sizeof dom->cd_fa_basic_node,
 						      &bt, M0_BCT_NO_CRC,
-						      EMBEDDED_RECORD, &b_op,
+						      EMBEDDED_INDIRECT, &b_op,
 						      dom->cd_fileattr_basic,
 						      seg, &fid, tx, &keycmp));
 	M0_ASSERT(rc == 0);
