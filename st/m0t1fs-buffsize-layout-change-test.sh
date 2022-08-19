431164ef86dfca0c92be7a819ad2fc557211cd33
Sayed Alfhad Shah <fahadshah2411@gmail.com>
2022-08-19 10:29:46 +0530

    CORTX-30751: Codacy code cleanup (#1606) (#1893)
    This patch fixes some of the codacy warnings.
    warning fixed : "Double quote to prevent globing and words splitting".
    
    Signed-off-by: alfhad <fahadshah2411@gmail.com>

--- st/m0t1fs-buffsize-layout-change-test.sh
+++ st/m0t1fs-buffsize-layout-change-test.sh
@@ -24,7 +24,7 @@ set -e
 SANDBOX_DIR=${SANDBOX_DIR:-/var/motr/sandbox.m0t1fs-writesize-st}
 M0_SRC_DIR=`readlink -f $0`
 M0_SRC_DIR=${M0_SRC_DIR%/*/*}
-cd $M0_SRC_DIR
+cd "$M0_SRC_DIR"
 
 echo "Installing Motr services"
 sudo scripts/install-motr-service -u
@@ -43,7 +43,7 @@ dd if=/dev/zero of=/mnt/m0t1fs/12345:1 bs=1048576 count=10
 stat /mnt/m0t1fs/12345:1
 blksize=`stat /mnt/m0t1fs/12345:1 | grep "IO Block" | sed -e 's/.*IO Block:[[:space:]]//' -e 's/[[:space:]]reg.*//'`
 
-if test $blksize -eq 1048576 -a $oldblksize -ne 1048576; then
+if test "$blksize" -eq 1048576 -a "$oldblksize" -ne 1048576; then
 	echo "Successfully set IO Block on first write"
 else
 	echo "IO Block size is not set correctly"
@@ -59,7 +59,7 @@ setfattr -n writesize -v "1048576" /mnt/m0t1fs/12345:2
 stat /mnt/m0t1fs/12345:2
 blksize=`stat /mnt/m0t1fs/12345:2 | grep "IO Block" | sed -e 's/.*IO Block:[[:space:]]//' -e 's/[[:space:]]reg.*//'`
 
-if test $blksize -eq 1048576 -a $oldblksize -ne 1048576; then
+if test "$blksize" -eq 1048576 -a "$oldblksize" -ne 1048576; then
 	echo "Successfully set IO Block on setfattr"
 else
 	echo "IO Block size is not set correctly"
