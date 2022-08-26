88581eacb0facfbdbe3a9f0f65753a07d3e8ea6f
Max <maksym.medvied@seagate.com>
2022-08-26 18:04:30 +0500

    CORTX-27600 dtm0/log: initial implementation for dtx0 and pruner (#1503)
    Problem:
    Currently, dtm0 log entries are stored in a simple list, which reduces
    the performance because for each pmsg we have to traverse this list to
    find the entry to update it with the information about the received pmsg.
    
    Solution:
    In the dtm0 log we store the entries in the btree.
    This solves the above issue.
    
    Also, we add a simple pruner which deletes the old records from the log
    if they are All-P (i.e. received pmsgs from all participants).
    Without pmsgs and redo_lists (TBD in next PRs), the pruner just deletes
    records from the log as soon as they are added there.
    
    In contrary to the old dtm0 code currently in main, which requires compilation
    option to be enabled, this code will be always ON.
    It will help us to debug the code earlier.
    
    The patch contains UTs for the log and the pruner.
    
    Signed-off-by: Maxim Medved max.medved@seagate.com
    Co-authored-by: Ivan Alekhin ivan.alekhin@seagate.com
    Co-authored-by: Madhavrao Vemuri madhav.vemuri@seagate.com
    Co-authored-by: Andriy Tkachuk andriy.tkachuk@seagate.com

---
+++ dtm0/dtm0.c
@@ -0,0 +1,145 @@
+/* -*- C -*- */
+/*
+ * Copyright (c) 2013-2020 Seagate Technology LLC and/or its Affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For any questions about this software or licensing,
+ * please email opensource@seagate.com or cortx-questions@seagate.com.
+ *
+ */
+
+/**
+ * @addtogroup dtm0
+ *
+ * @{
+ */
+
+#define M0_TRACE_SUBSYSTEM M0_TRACE_SUBSYS_DTM0
+#include "lib/trace.h"
+
+#include "dtm0/dtm0.h"
+#include "dtm0/clk_src.h"
+#include "dtm0/pruner.h"
+#include "lib/memory.h" /* M0_ALLOC_ARR */
+#include "lib/errno.h" /* ENOMEM */
+
+M0_INTERNAL bool m0_dtx0_id_eq(const struct m0_dtx0_id *left,
+			       const struct m0_dtx0_id *right)
+{
+	return m0_dtx0_id_cmp(left, right) == 0;
+}
+
+M0_INTERNAL int m0_dtx0_id_cmp(const struct m0_dtx0_id *left,
+			       const struct m0_dtx0_id *right)
+{
+	return M0_3WAY(left->dti_timestamp, right->dti_timestamp) ?:
+		m0_fid_cmp(&left->dti_originator_sdev_fid,
+			   &right->dti_originator_sdev_fid);
+}
+
+static int descriptor_copy(struct m0_dtx0_descriptor *dst,
+			   const struct m0_dtx0_descriptor *src)
+{
+	struct m0_fid *dst_arr;
+	struct m0_fid *src_arr = src->dtd_participants.dtpa_participants;
+	uint64_t       nr = src->dtd_participants.dtpa_participants_nr;
+
+	if (nr == 0) {
+		M0_SET0(dst);
+		return 0;
+	}
+
+	M0_ALLOC_ARR(dst_arr, nr);
+	if (dst_arr == NULL)
+		return M0_ERR(-ENOMEM);
+
+	memcpy(dst_arr, src_arr, sizeof(*dst_arr) * nr);
+
+	dst->dtd_participants.dtpa_participants = dst_arr;
+	dst->dtd_participants.dtpa_participants_nr = nr;
+	dst->dtd_id = src->dtd_id;
+	return 0;
+}
+
+static int buf2bufs_copy(struct m0_bufs *dst, const struct m0_buf *src)
+{
+	struct m0_buf *dst_buf;
+	int            rc;
+
+	M0_ALLOC_PTR(dst_buf);
+	if (dst_buf == NULL)
+		return M0_ERR(-ENOMEM);
+	rc = m0_buf_copy(dst_buf, src);
+	if (rc != 0) {
+		m0_free(dst_buf);
+		return M0_ERR(rc);
+	}
+	dst->ab_count = 1;
+	dst->ab_elems = dst_buf;
+	return 0;
+}
+
+M0_INTERNAL int
+m0_dtm0_redo_init(struct m0_dtm0_redo *redo,
+		  const struct m0_dtx0_descriptor *descriptor,
+		  const struct m0_buf             *payload,
+		  enum m0_dtx0_payload_type        type)
+{
+	int rc;
+
+	redo->dtr_payload.dtp_type = type;
+	M0_LOG(M0_DEBUG, "dtx id: " DTID1_F "participants:%d",
+		DTID1_P(&descriptor->dtd_id),
+		(int)descriptor->dtd_participants.dtpa_participants_nr);
+	rc = descriptor_copy(&redo->dtr_descriptor, descriptor) ?:
+		buf2bufs_copy(&redo->dtr_payload.dtp_data, payload);
+	if (rc != 0)
+		m0_dtm0_redo_fini(redo);
+	return rc;
+}
+
+M0_INTERNAL void m0_dtm0_redo_fini(struct m0_dtm0_redo *redo)
+{
+	m0_free(redo->dtr_descriptor.dtd_participants.dtpa_participants);
+	m0_bufs_free(&redo->dtr_payload.dtp_data);
+}
+
+M0_INTERNAL int  m0_dtm0_mod_init(void)
+{
+	/* TODO: Register stype here (and other types). */
+	m0_dtm0_pruner_mod_init();
+	return 0;
+}
+
+M0_INTERNAL void m0_dtm0_mod_fini(void)
+{
+	m0_dtm0_pruner_mod_fini();
+}
+
+#undef M0_TRACE_SUBSYSTEM
+
+/** @} end of dtm0 group */
+
+/*
+ *  Local variables:
+ *  c-indentation-style: "K&R"
+ *  c-basic-offset: 8
+ *  tab-width: 8
+ *  fill-column: 80
+ *  scroll-step: 1
+ *  End:
+ */
+/*
+ * vim: tabstop=8 shiftwidth=8 noexpandtab textwidth=80 nowrap
+ */
