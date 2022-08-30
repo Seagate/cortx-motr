bb0c07c63ed26326c65c3a9aafcda4a15812f6e3
Abhishek Saha <abhishek.saha@seagate.com>
2022-08-29 23:50:17 -0600

    addressed vinoth's review comments
    Signed-off-by: Abhishek Saha <abhishek.saha@seagate.com>

--- rpc/conn.c
+++ rpc/conn.c
@@ -1433,7 +1433,8 @@ M0_INTERNAL int m0_rpc_conn_ha_timer_start(struct m0_rpc_conn *conn)
 		return M0_RC(0); /* there's no point to arm the timer */
 	if (m0_sm_timer_is_armed(&conn->c_ha_timer))
 		return M0_RC(0); /* Already started */
-	else if (conn->c_ha_timer.tr_timer.t_state != M0_TIMER_UNINIT)
+	else if (M0_IN(conn->c_ha_timer.tr_timer.t_state,
+			(M0_TIMER_STOPPED, M0_TIMER_INITED)))
 		m0_sm_timer_fini(&conn->c_ha_timer);
 	if (conn->c_rpc_machine->rm_stopping)
 		return M0_RC(0);
@@ -1453,8 +1454,10 @@ M0_INTERNAL void m0_rpc_conn_ha_timer_stop(struct m0_rpc_conn *conn)
 	if (m0_sm_timer_is_armed(&conn->c_ha_timer)) {
 		M0_LOG(M0_DEBUG, "Cancelling HA timer; rpc conn=%p", conn);
 		m0_sm_timer_cancel(&conn->c_ha_timer);
-		m0_sm_timer_fini(&conn->c_ha_timer);
 	}
+	else if (M0_IN(conn->c_ha_timer.tr_timer.t_state,
+			(M0_TIMER_STOPPED, M0_TIMER_INITED)))
+		m0_sm_timer_fini(&conn->c_ha_timer);
 }
 
 /**
