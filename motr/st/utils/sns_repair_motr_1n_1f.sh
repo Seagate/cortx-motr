762559a51c0e584c0f0d2340595f37fa634ccd89
Zoheb Khan <zoheb.khan@seagate.com>
2022-09-16 05:04:41 -0600

    This patch fixes some of the codacy warnings. warning fixed: "Double quote to prevent globing and words splitting". warning fixed: "Use $(...) notation instead of legacy backticked ...."
    Co-authored-by: Pradeep Kumbhre pradeep.kumbhre@seagate.com
    Signed-off-by: Zoheb Khan <zoheb.khan@seagate.com>

--- motr/st/utils/sns_repair_motr_1n_1f.sh
+++ motr/st/utils/sns_repair_motr_1n_1f.sh
@@ -19,14 +19,14 @@
 #
 
 
-TOPDIR=`dirname $0`/../../../
+TOPDIR=$(dirname "$0")/../../../
 
-. ${TOPDIR}/m0t1fs/linux_kernel/st/common.sh
-. ${TOPDIR}/m0t1fs/linux_kernel/st/m0t1fs_common_inc.sh
-. ${TOPDIR}/m0t1fs/linux_kernel/st/m0t1fs_client_inc.sh
-. ${TOPDIR}/m0t1fs/linux_kernel/st/m0t1fs_server_inc.sh
-. ${TOPDIR}/m0t1fs/linux_kernel/st/m0t1fs_sns_common_inc.sh
-. ${TOPDIR}/motr/st/utils/sns_repair_common_inc.sh
+. "${TOPDIR}"/m0t1fs/linux_kernel/st/common.sh
+. "${TOPDIR}"/m0t1fs/linux_kernel/st/m0t1fs_common_inc.sh
+. "${TOPDIR}"/m0t1fs/linux_kernel/st/m0t1fs_client_inc.sh
+. "${TOPDIR}"/m0t1fs/linux_kernel/st/m0t1fs_server_inc.sh
+. "${TOPDIR}"/m0t1fs/linux_kernel/st/m0t1fs_sns_common_inc.sh
+. "${TOPDIR}"/motr/st/utils/sns_repair_common_inc.sh
 
 
 export MOTR_CLIENT_ONLY=1
@@ -111,12 +111,12 @@ main()
 
 	sandbox_init
 
-	NODE_UUID=`uuidgen`
+	NODE_UUID=$(uuidgen)
 	local multiple_pools=0
 
 	SINGLE_NODE=1
 
-	motr_service start $multiple_pools $stride $N $K $S $P || {
+	motr_service start $multiple_pools "$stride" "$N" "$K" "$S" "$P" || {
 		echo "Failed to start Motr Service."
 		SINGLE_NODE=0
 		return 1
